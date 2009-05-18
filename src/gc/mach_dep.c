07fa02ad7f70b21e0797671678aa7b665e95e6f0
Juan Jose Garcia Ripoll <jjgarcia@juanjo-imac.local>
2009-05-18 17:27:13 +0200

    Linux/mips is missing getcontext. Temporary fix for gc.

--- src/gc/mach_dep.c
+++ src/gc/mach_dep.c
@@ -175,7 +175,7 @@ void GC_with_callee_saves_pushed(void (*fn)(ptr_t, void *),
 #   if defined(HAVE_PUSH_REGS)
       GC_push_regs();
 #   elif defined(UNIX_LIKE) && !defined(DARWIN) && !defined(ARM32) && \
-	 !defined(HURD)
+	 !defined(HURD) && !defined(__mips__) /* JJGR (added !__mips__) */
       /* Older versions of Darwin seem to lack getcontext(). */
       /* ARM Linux often doesn't support a real getcontext(). */
       ucontext_t ctxt;
