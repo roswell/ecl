a692c9c50f47f55322038d7f7fee05df4e5f8138
Juan Jose Garcia Ripoll <jjgarcia@jjgr-2.local>
2009-05-20 20:11:52 +0200

    We had introduced redundant lines: si_{set,get}_limit already had support for changing the heap size.

--- src/c/stacks.d
+++ src/c/stacks.d
@@ -486,10 +486,6 @@ si_set_limit(cl_object type, cl_object size)
 		cs_set_size(env, the_size);
 	} else if (type == @'ext::lisp-stack') {
 		ecl_stack_set_size(env, the_size);
-#ifdef GBC_BOEHM
-	} else if (type == @'ext::heap-size') {
-                si_heap_size(1, size);
-#endif
         } else {
 		_ecl_set_max_heap_size(the_size);
 	}
@@ -509,10 +505,6 @@ si_get_limit(cl_object type)
 		output = env->cs_size;
 	} else if (type == @'ext::lisp-stack') {
 		output = env->stack_size;
-#ifdef GBC_BOEHM
-	} else if (type == @'ext::heap-size') {
-                output = si_heap_size(0);
-#endif
 	} else {
 		output = cl_core.max_heap_size;
 	}
