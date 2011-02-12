77f51dc653b0229a844a2323144e6644f0e58300
Juan Jose Garcia Ripoll <jjgarcia@users.sourceforge.net>
2011-02-12 19:35:41 +0100

    Remove printf statement FElibc_error().

--- src/c/error.d
+++ src/c/error.d
@@ -487,7 +487,6 @@ FElibc_error(const char *msg, int narg, ...)
 	cl_va_start(args, narg, narg, 0);
 	rest = cl_grab_rest_args(args);
 
-        printf("%s\n", error);
 	FEerror("~?~%Explanation: ~A.", 3, make_constant_base_string(msg), rest,
 		make_constant_base_string(error));
 }
