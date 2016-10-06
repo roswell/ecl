429a99a2c48695c8abd101dac21d30e743433dd7
Daniel Kochmański <daniel@turtleware.eu>
2016-10-06 11:43:21 +0200

    fix declaration of not_a_file_stream
    not_a_file_stream function is noreturn, so it doesn't matter what type
    for return is declared, but is used often in a context, where declared
    function returns cl_object. Make it happen to get rid of unnecessary
    warnings.

--- src/c/file.d
+++ src/c/file.d
@@ -74,7 +74,7 @@ static cl_object alloc_stream();
 
 static void cannot_close(cl_object stream) ecl_attr_noreturn;
 static void file_libc_error(cl_object error_type, cl_object stream, const char *msg, int narg, ...) ecl_attr_noreturn;
-static void not_a_file_stream(cl_object fn) ecl_attr_noreturn;
+static cl_object not_a_file_stream(cl_object fn) ecl_attr_noreturn;
 static void not_an_input_stream(cl_object fn) ecl_attr_noreturn;
 static void not_an_output_stream(cl_object fn) ecl_attr_noreturn;
 static void not_a_character_stream(cl_object s) ecl_attr_noreturn;
@@ -5418,7 +5418,7 @@ alloc_stream()
  * ERROR MESSAGES
  */
 
-static void
+static cl_object
 not_a_file_stream(cl_object strm)
 {
   cl_error(9, @'simple-type-error', @':format-control',
