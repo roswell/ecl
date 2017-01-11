roswell/ecl

commit 5758af7756ec52bac2e59485240f8cdb8766a916

msvc: Compare thread id instead of handlers to check if it is already registered.

src/c/threads/process.d
@@ -363,7 +363,9 @@ ecl_import_current_thread(cl_object name, cl_object bindings)
     cl_index i, size;
     for (i = 0, size = processes->vector.fillp; i < size; i++) {
       cl_object p = processes->vector.self.t[i];
-      if (!Null(p) && p->process.thread == current)
+      if (!Null(p)
+          &&
+          GetThreadId(p->process.thread) == GetThreadId(current))
         return 0;
     }
   }

commit 4ed1ba10cc9b60fcddfab160c80e80e67a9152d3

Fix non msvc build broken by 5758af77

src/c/threads/process.d
@@ -365,7 +365,12 @@ ecl_import_current_thread(cl_object name, cl_object bindings)
       cl_object p = processes->vector.self.t[i];
       if (!Null(p)
           &&
-          GetThreadId(p->process.thread) == GetThreadId(current))
+#ifdef ECL_WINDOWS_THREADS
+          GetThreadId(p->process.thread) == GetThreadId(current)
+#else
+	      p->process.thread == current
+#endif
+	      )
         return 0;
     }
   }