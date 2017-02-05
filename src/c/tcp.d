10e3b8c39db0da62bf8e0bacce60c80aad6ccf14
Kris Katterjohn <katterjohn@gmail.com>
2017-02-05 13:02:55 -0600

    Fix error message in OPEN-SERVER-STREAM
    It mentioned OPEN-CLIENT-STREAM instead.

--- src/c/tcp.d
+++ src/c/tcp.d
@@ -312,7 +312,7 @@ si_open_server_stream(cl_object port)
   if (ecl_unlikely(!ECL_FIXNUMP(port) ||
                    ecl_fixnum_minusp(port) ||
                    ecl_fixnum_greater(port,ecl_make_fixnum(65535)))) {
-    FEwrong_type_only_arg(@[si::open-client-stream], port,
+    FEwrong_type_only_arg(@[si::open-server-stream], port,
                           ecl_read_from_cstring("(INTEGER 0 65535)"));
   }
   p = ecl_fixnum(port);
