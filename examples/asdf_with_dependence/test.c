fb7117a171fa981ef331b33928b916fec4df06a6
Bo Yao <icerove@gmail.com>
2017-03-15 13:08:34 -0400

    add an example for build asdf with dependence

---
+++ examples/asdf_with_dependence/test.c
@@ -0,0 +1,14 @@
+#include <ecl/ecl.h>
+
+int main (int argc, char **argv) {
+  extern void init_dll_EXAMPLE_WITH_DEP__ALL_SYSTEMS(cl_object);
+  
+  cl_boot(argc, argv);
+  ecl_init_module(NULL, init_dll_EXAMPLE_WITH_DEP__ALL_SYSTEMS);
+
+  /* do things with the Lisp library */
+  cl_eval(c_string_to_object("(example:test-function 5)"));
+
+  cl_shutdown();
+  return 0;
+}
