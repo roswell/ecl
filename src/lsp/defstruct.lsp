roswell/ecl

commit ba573abf9eb97820e3b958792e200f40a1c3fa8d

defstruct: don't use eval in define-structure

src/lsp/defstruct.lsp
@@ -308,33 +308,36 @@
   (create-type-name name)
   ;; We are going to modify this list!!!
   (setf slot-descriptions (copy-tree slot-descriptions))
-  ;; FIXME! We could do the same with ENSURE-CLASS!
   #+clos
   (unless type
-    (eval `(defclass ,name ,(and include (list include))
-             ,(mapcar
-               #'(lambda (sd)
-                   (if sd
-                       (list* (first sd)
-                              :initform (second sd)
-                              :initarg 
-                              (intern (symbol-name (first sd))
-                                      (find-package 'KEYWORD))
-                              (when (third sd) (list :type (third sd))))
-                       nil))            ; for initial offset slots
-               slot-descriptions)
-             (:metaclass structure-class))))
-  ;; FIXME! We can do the same with INSTALL-METHOD!
+    (clos:ensure-class
+     name
+     :direct-superclasses (and include (list include))
+     :direct-slots (mapcar
+                    #'(lambda (sd)
+                        (if sd
+                            (list* :name (first sd)
+                                   :initform (second sd)
+                                   :initargs
+                                   (list
+                                    (intern (symbol-name (first sd))
+                                            (find-package 'KEYWORD)))
+                                   (when (third sd) (list :type (third sd))))
+                            nil))            ; for initial offset slots
+                    slot-descriptions)
+     :metaclass 'structure-class))
   #+clos
   (when print-function
-    (eval `(defmethod print-object ((obj ,name) stream)
-             (,print-function obj stream 0)
-             obj)))
+    (clos::install-method 'print-object nil (list name t) '(obj stream)
+                          #'(lambda (obj stream)
+                              (funcall print-function obj stream 0)
+                              obj)))
   #+clos
   (when print-object
-    (eval `(defmethod print-object ((obj ,name) stream)
-            (,print-object obj stream)
-            obj)))
+    (clos::install-method 'print-object nil (list name t) '(obj stream)
+                          #'(lambda (obj stream)
+                              (funcall print-object obj stream)
+                              obj)))
   (when predicate
     (fset predicate (make-predicate name type named name-offset)))
   (put-sysprop name 'DEFSTRUCT-FORM `(defstruct ,name ,@slots))

commit 0407b9697c8a31ea9b773942b250e3bf9c0cc0e2

defstruct: fix print-object and print-function options for lambdas
Bug introduced in commit ba573abf9eb97820e3b958792e200f40a1c3fa8d.

src/lsp/defstruct.lsp
@@ -450,8 +450,12 @@ as a STRUCTURE doc and can be retrieved by (documentation 'NAME 'structure)."
                 (setq include (cdar os))
                 (unless (get-sysprop v 'IS-A-STRUCTURE)
                   (error "~S is an illegal included structure." v)))
-               (:PRINT-FUNCTION (setq print-function v))
-               (:PRINT-OBJECT (setq print-object v))
+               (:PRINT-FUNCTION (setq print-function (if (symbolp v)
+                                                         `(quote ,v)
+                                                         `(function ,v))))
+               (:PRINT-OBJECT (setq print-object (if (symbolp v)
+                                                     `(quote ,v)
+                                                     `(function ,v))))
                (:TYPE (setq type v))
                (:INITIAL-OFFSET (setq initial-offset v))
                (t (error "~S is an illegal defstruct option." o))))
@@ -562,7 +566,7 @@ as a STRUCTURE doc and can be retrieved by (documentation 'NAME 'structure)."
     ;;
     (let ((core `(define-structure ',name ',conc-name ',type ',named ',slots
                                    ',slot-descriptions ',copier ',include
-                                   ',print-function ',print-object ',constructors
+                                   ,print-function ,print-object ',constructors
                                    ',offset ',name-offset
                                    ',documentation ',predicate))
           (constructors (mapcar #'(lambda (constructor)