d7b2dfb7227da90036c2c148535b8d1ce74d8be4
Marius Gerbershagen <marius.gerbershagen@gmail.com>
2019-12-26 15:42:00 +0100

    Change handling of C stack size
    Previously: unconditionally use the size from ECL_OPT_C_STACK_SIZE if
    it is larger than the OS provided value and if possible resize the
    stack at runtime.
    
    Now: Use ECL_OPT_C_STACK_SIZE if provided. Otherwise, use a) the OS
    provided value if we can resize the stack at runtime (for Unix) or b)
    the new build option ECL_DEFAULT_C_STACK_SIZE (1 MB) if we can set the
    stack size at link time (for Windows).

--- src/c/main.d
+++ src/c/main.d
@@ -92,7 +92,7 @@ cl_fixnum ecl_option_values[ECL_OPT_LIMIT+1] = {
   128,            /* ECL_OPT_FRAME_STACK_SAFETY_AREA */
   32768,          /* ECL_OPT_LISP_STACK_SIZE */
   128,            /* ECL_OPT_LISP_STACK_SAFETY_AREA */
-  128*sizeof(cl_index)*1024, /* ECL_OPT_C_STACK_SIZE */
+  ECL_DEFAULT_C_STACK_SIZE, /* ECL_OPT_C_STACK_SIZE */
   4*sizeof(cl_index)*1024, /* ECL_OPT_C_STACK_SAFETY_AREA */
   HEAP_SIZE_DEFAULT, /* ECL_OPT_HEAP_SIZE */
   1024*1024,      /* ECL_OPT_HEAP_SAFETY_AREA */
