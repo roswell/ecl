roswell/ecl

commit 0102aed9f57f69b8cb75d518fc8c701cb437b1d8

We introduce two functions, ecl_to_fix/ecl_to_size, which may be inlined and are more focused than fixint/fixnnint

src/c/big.d
@@ -298,6 +298,36 @@ mp_free(void *ptr, size_t size)
         ecl_dealloc(ptr);
 }
 
+cl_fixnum
+fixint(cl_object x)
+{
+        if (FIXNUMP(x))
+                return ecl_fix(x);
+        if (ECL_BIGNUMP(x)) {
+                if (mpz_fits_slong_p(x->big.big_num)) {
+                        return mpz_get_si(x->big.big_num);
+                }
+        }
+	FEwrong_type_argument(@[fixnum], x);
+}
+
+cl_index
+fixnnint(cl_object x)
+{
+        if (FIXNUMP(x)) {
+                cl_fixnum i = ecl_fix(x);
+                if (i >= 0)
+                        return i;
+        } else if (ECL_BIGNUMP(x)) {
+                if (mpz_fits_ulong_p(x->big.big_num)) {
+                        return mpz_get_ui(x->big.big_num);
+                }
+        }
+	FEwrong_type_argument(cl_list(3, @'integer', MAKE_FIXNUM(0),
+				      MAKE_FIXNUM(MOST_POSITIVE_FIXNUM)),
+			      x);
+}
+
 #undef _ecl_big_set_fixnum
 #undef _ecl_big_set_index
 #if ECL_LONG_BITS >= FIXNUM_BITS
@@ -325,12 +355,6 @@ cl_index
 _ecl_big_get_index(cl_object x)
 {
 	return mpz_get_ui((x)->big.big_num);
-}
-
-bool
-_ecl_big_fits_in_index(cl_object x)
-{
-	return mpz_fits_ulong_p(x->big.big_num);
 }
 #elif GMP_LIMB_BITS >= FIXNUM_BITS
 cl_object

src/c/alloc.d
@@ -798,7 +798,7 @@ t_from_type(cl_object type)
 	cl_index i;
 @
 	tm = tm_of(t_from_type(type));
-	i = fixnnint(qty);
+	i = ecl_to_size(qty);
 	if (tm->tm_npage > i) i = tm->tm_npage;
 	tm->tm_maxpage = i;
 	if (now == Cnil || tm->tm_maxpage <= tm->tm_npage)
@@ -826,7 +826,7 @@ t_from_type(cl_object type)
 	cl_index i, m;
 	cl_ptr p;
 @
-	i = fixnnint(qty);
+	i = ecl_to_size(qty);
 	if (ncbpage > i)
 	  FEerror("Can't set the limit for contiguous blocks to ~D,~%\
 since ~D pages are already allocated.",
@@ -863,7 +863,7 @@ since ~D pages are already allocated.",
 @(defun si::set-hole-size (size)
 	cl_index i;
 @
-	i = fixnnint(size);
+	i = ecl_to_size(size);
 	if (i == 0 || i > available_pages() + new_holepage)
 	  FEerror("Illegal value for the hole size.", 0);
 	new_holepage = i;

src/c/array.d
@@ -112,14 +112,14 @@ ecl_to_index(cl_object n)
 cl_object
 cl_row_major_aref(cl_object x, cl_object indx)
 {
-	cl_index j = fixnnint(indx);
+	cl_index j = ecl_to_size(indx);
 	@(return ecl_aref(x, j))
 }
 
 cl_object
 si_row_major_aset(cl_object x, cl_object indx, cl_object val)
 {
-	cl_index j = fixnnint(indx);
+	cl_index j = ecl_to_size(indx);
 	@(return ecl_aset(x, j, val))
 }
 
@@ -325,10 +325,10 @@ ecl_aset_unsafe(cl_object x, cl_index index, cl_object value)
 		break;
 	}
 	case aet_fix:
-		x->array.self.fix[index] = fixint(value);
+		x->array.self.fix[index] = ecl_to_fix(value);
 		break;
 	case aet_index:
-		x->array.self.index[index] = fixnnint(value);
+		x->array.self.index[index] = ecl_to_size(value);
 		break;
 	case aet_sf:
 		x->array.self.sf[index] = ecl_to_float(value);
@@ -854,7 +854,7 @@ cl_array_rank(cl_object a)
 cl_object
 cl_array_dimension(cl_object a, cl_object index)
 {
-	@(return MAKE_FIXNUM(ecl_array_dimension(a, fixnnint(index))))
+	@(return MAKE_FIXNUM(ecl_array_dimension(a, ecl_to_size(index))))
 }
 
 cl_index
@@ -1264,9 +1264,9 @@ cl_object
 si_copy_subarray(cl_object dest, cl_object start0,
                  cl_object orig, cl_object start1, cl_object length)
 {
-        ecl_copy_subarray(dest, fixnnint(start0),
-                          orig, fixnnint(start1),
-                          fixnnint(length));
+        ecl_copy_subarray(dest, ecl_to_size(start0),
+                          orig, ecl_to_size(start1),
+                          ecl_to_size(length));
         @(return dest)
 }
 
@@ -1274,8 +1274,8 @@ cl_object
 si_fill_array_with_elt(cl_object x, cl_object elt, cl_object start, cl_object end)
 {
 	cl_elttype t = ecl_array_elttype(x);
-        cl_index first = fixnnint(start);
-        cl_index last = Null(end)? x->array.dim : fixnnint(end);
+        cl_index first = ecl_to_size(start);
+        cl_index last = Null(end)? x->array.dim : ecl_to_size(end);
         if (first >= last) {
                 goto END;
         }
@@ -1300,13 +1300,13 @@ si_fill_array_with_elt(cl_object x, cl_object elt, cl_object start, cl_object en
         }
 #endif
 	case aet_fix: {
-                cl_fixnum e = fixint(elt);
+                cl_fixnum e = ecl_to_fix(elt);
                 cl_fixnum *p = x->vector.self.fix + first;
 		for (first = last - first; first; --first, ++p) { *p = e; }
 		break;
         }
 	case aet_index: {
-                cl_index e = fixnnint(elt);
+                cl_index e = ecl_to_size(elt);
                 cl_index *p = x->vector.self.index + first;
 		for (first = last - first; first; --first, ++p) { *p = e; }
 		break;

src/c/ffi.d
@@ -250,7 +250,7 @@ cl_object
 si_allocate_foreign_data(cl_object tag, cl_object size)
 {
 	cl_object output = ecl_alloc_object(t_foreign);
-	cl_index bytes = fixnnint(size);
+	cl_index bytes = ecl_to_size(size);
 	output->foreign.tag = tag;
 	output->foreign.size = bytes;
 	/* FIXME! Should be atomic uncollectable or malloc, but we do not export
@@ -337,8 +337,8 @@ cl_object
 si_foreign_data_pointer(cl_object f, cl_object andx, cl_object asize,
 			cl_object tag)
 {
-	cl_index ndx = fixnnint(andx);
-	cl_index size = fixnnint(asize);
+	cl_index ndx = ecl_to_size(andx);
+	cl_index size = ecl_to_size(asize);
 	cl_object output;
 
 	if (ecl_unlikely(type_of(f) != t_foreign)) {
@@ -358,8 +358,8 @@ si_foreign_data_pointer(cl_object f, cl_object andx, cl_object asize,
 cl_object
 si_foreign_data_ref(cl_object f, cl_object andx, cl_object asize, cl_object tag)
 {
-	cl_index ndx = fixnnint(andx);
-	cl_index size = fixnnint(asize);
+	cl_index ndx = ecl_to_size(andx);
+	cl_index size = ecl_to_size(asize);
 	cl_object output;
 
 	if (ecl_unlikely(type_of(f) != t_foreign)) {
@@ -377,7 +377,7 @@ si_foreign_data_ref(cl_object f, cl_object andx, cl_object asize, cl_object tag)
 cl_object
 si_foreign_data_set(cl_object f, cl_object andx, cl_object value)
 {
-	cl_index ndx = fixnnint(andx);
+	cl_index ndx = ecl_to_size(andx);
 	cl_index size, limit;
 
 	if (ecl_unlikely(type_of(f) != t_foreign)) {
@@ -537,37 +537,35 @@ ecl_foreign_data_set_elt(void *p, enum ecl_ffi_tag tag, cl_object value)
 		*(unsigned char*)p = (unsigned char)ecl_base_char_code(value);
 		break;
 	case ECL_FFI_BYTE:
-		*(int8_t *)p = fixint(value);
+		*(int8_t *)p = ecl_to_int8_t(value);
 		break;
 	case ECL_FFI_UNSIGNED_BYTE:
-		*(uint8_t *)p = fixnnint(value);
+		*(uint8_t *)p = ecl_to_uint8_t(value);
 		break;
 	case ECL_FFI_SHORT:
-		*(short *)p = fixint(value);
+		*(short *)p = ecl_to_short(value);
 		break;
 	case ECL_FFI_UNSIGNED_SHORT:
-		*(unsigned short *)p = fixnnint(value);
+		*(unsigned short *)p = ecl_to_ushort(value);
 		break;
 	case ECL_FFI_INT:
-		*(int *)p = fixint(value);
+		*(int *)p = ecl_to_int(value);
 		break;
 	case ECL_FFI_UNSIGNED_INT:
-		*(unsigned int *)p = fixnnint(value);
+		*(unsigned int *)p = ecl_to_uint(value);
 		break;
 	case ECL_FFI_LONG:
-		*(long *)p = fixint(value);
+		*(long *)p = ecl_to_long(value);
 		break;
 	case ECL_FFI_UNSIGNED_LONG:
-		*(unsigned long *)p = fixnnint(value);
+		*(unsigned long *)p = ecl_to_ulong(value);
 		break;
-#ifdef ecl_uint8_t
         case ECL_FFI_INT8_T:
-                *(ecl_int8_t *)p = fixint(value);
+                *(ecl_int8_t *)p = ecl_to_int8_t(value);
                 break;
         case ECL_FFI_UINT8_T:
-                *(ecl_uint8_t *)p = fixnnint(value);
+                *(ecl_uint8_t *)p = ecl_to_uint8_t(value);
                 break;
-#endif
 #ifdef ecl_uint16_t
         case ECL_FFI_INT16_T:
                 *(ecl_int16_t *)p = ecl_to_int16_t(value);
@@ -625,7 +623,7 @@ ecl_foreign_data_set_elt(void *p, enum ecl_ffi_tag tag, cl_object value)
 cl_object
 si_foreign_data_ref_elt(cl_object f, cl_object andx, cl_object type)
 {
-	cl_index ndx = fixnnint(andx);
+	cl_index ndx = ecl_to_size(andx);
 	cl_index limit = f->foreign.size;
 	enum ecl_ffi_tag tag = ecl_foreign_type_code(type);
 	if (ecl_unlikely(ndx >= limit ||
@@ -642,7 +640,7 @@ si_foreign_data_ref_elt(cl_object f, cl_object andx, cl_object type)
 cl_object
 si_foreign_data_set_elt(cl_object f, cl_object andx, cl_object type, cl_object value)
 {
-	cl_index ndx = fixnnint(andx);
+	cl_index ndx = ecl_to_size(andx);
 	cl_index limit = f->foreign.size;
 	enum ecl_ffi_tag tag = ecl_foreign_type_code(type);
 	if (ecl_unlikely(ndx >= limit ||
@@ -692,7 +690,7 @@ si_foreign_data_recast(cl_object f, cl_object size, cl_object tag)
 	if (ecl_unlikely(type_of(f) != t_foreign))
                 FEwrong_type_nth_arg(@[si::foreign-data-recast], 1, f,
                                      @[si::foreign-data]);
-	f->foreign.size = fixnnint(size);
+	f->foreign.size = ecl_to_size(size);
 	f->foreign.tag = tag;
 	@(return f)
 }

src/c/file.d
@@ -313,7 +313,7 @@ generic_read_byte_unsigned8(cl_object strm)
 static void
 generic_write_byte_unsigned8(cl_object byte, cl_object strm)
 {
-	unsigned char c = fixnnint(byte);
+	unsigned char c = ecl_to_uint8_t(byte);
 	strm->stream.ops->write_byte8(strm, &c, 1);
 }
 
@@ -1279,7 +1279,7 @@ clos_stream_column(cl_object strm)
 	/* FIXME! The Gray streams specifies NIL is a valid
 	 * value but means "unknown". Should we make it
 	 * zero? */
-	return Null(col)? 0 : fixnnint(col);
+	return Null(col)? 0 : ecl_to_size(col);
 }
 
 static cl_object
@@ -1363,7 +1363,7 @@ str_out_set_position(cl_object strm, cl_object pos)
 	if (Null(pos)) {
 		disp = strm->base_string.dim;
 	} else {
-		disp = fixnnint(pos);
+		disp = ecl_to_size(pos);
 	}
 	if (disp < string->base_string.fillp) {
 		string->base_string.fillp = disp;
@@ -1560,7 +1560,7 @@ str_in_set_position(cl_object strm, cl_object pos)
 	if (Null(pos)) {
 		disp = STRING_INPUT_LIMIT(strm);
 	}  else {
-		disp = fixnnint(pos);
+		disp = ecl_to_size(pos);
 		if (disp >= STRING_INPUT_LIMIT(strm)) {
 			disp = STRING_INPUT_LIMIT(strm);
 		}
@@ -4006,7 +4006,7 @@ seq_in_set_position(cl_object strm, cl_object pos)
 	if (Null(pos)) {
 		disp = SEQ_INPUT_LIMIT(strm);
 	}  else {
-		disp = fixnnint(pos);
+		disp = ecl_to_size(pos);
 		if (disp >= SEQ_INPUT_LIMIT(strm)) {
 			disp = SEQ_INPUT_LIMIT(strm);
 		}
@@ -4170,7 +4170,7 @@ seq_out_set_position(cl_object strm, cl_object pos)
 	if (Null(pos)) {
 		disp = SEQ_OUTPUT_LIMIT(strm);
 	}  else {
-		disp = fixnnint(pos);
+		disp = ecl_to_size(pos);
 		if (disp >= SEQ_OUTPUT_LIMIT(strm)) {
 			disp = SEQ_OUTPUT_LIMIT(strm);
 		}
@@ -4780,9 +4780,9 @@ ecl_normalize_stream_element_type(cl_object element_type)
 	}
 	if (CONSP(element_type)) {
 		if (CAR(element_type) == @'unsigned-byte')
-			return fixnnint(cl_cadr(element_type));
+			return ecl_to_size(cl_cadr(element_type));
 		if (CAR(element_type) == @'signed-byte')
-			return -fixnnint(cl_cadr(element_type));
+			return -ecl_to_size(cl_cadr(element_type));
 	}
 	for (size = 8; 1; size++) {
 		cl_object type;

src/c/instance.d
@@ -31,7 +31,7 @@ ecl_allocate_instance(cl_object clas, cl_index size)
 cl_object
 si_allocate_raw_instance(cl_object orig, cl_object clas, cl_object size)
 {
-	cl_object output = ecl_allocate_instance(clas, fixnnint(size));
+	cl_object output = ecl_allocate_instance(clas, ecl_to_size(size));
 	if (orig == Cnil) {
 		orig = output;
 	} else {

src/c/list.d
@@ -403,14 +403,14 @@ ecl_last(cl_object l, cl_index n)
 @
 	if (type_of(k) == t_bignum)
 		@(return l)
-	@(return ecl_last(l, fixnnint(k)))
+	@(return ecl_last(l, ecl_to_size(k)))
 @)
 
 @(defun make_list (size &key initial_element &aux x)
 	cl_fixnum i;
 @
-	/* INV: fixnnint() signals a type-error if SIZE is not a integer >=0 */
-	i = fixnnint(size);
+	/* INV: ecl_to_size() signals a type-error if SIZE is not a integer >=0 */
+	i = ecl_to_size(size);
 	while (i-- > 0)
 		x = CONS(initial_element, x);
 	@(return x)
@@ -578,8 +578,8 @@ ecl_butlast(cl_object l, cl_index n)
 	/* INV: No list has more than MOST_POSITIVE_FIXNUM elements */
 	if (type_of(nn) == t_bignum)
 		@(return Cnil);
-	/* INV: fixnnint() signals a type-error if NN is not an integer >=0 */
-	@(return ecl_butlast(lis, fixnnint(nn)))
+	/* INV: ecl_to_size() signals a type-error if NN is not an integer >=0 */
+	@(return ecl_butlast(lis, ecl_to_size(nn)))
 @)
 
 cl_object
@@ -607,8 +607,8 @@ ecl_nbutlast(cl_object l, cl_index n)
 	/* INV: No list has more than MOST_POSITIVE_FIXNUM elements */
 	if (type_of(nn) == t_bignum)
 		@(return Cnil)
-	/* INV: fixnnint() signas a type-error if NN is not an integer >=0 */
-	@(return ecl_nbutlast(lis, fixnnint(nn)))
+	/* INV: ecl_to_size() signas a type-error if NN is not an integer >=0 */
+	@(return ecl_nbutlast(lis, ecl_to_size(nn)))
 @)
 
 cl_object

src/c/num_log.d
@@ -379,7 +379,7 @@ cl_logbitp(cl_object p, cl_object x)
 
 	assert_type_integer(x);
 	if (FIXNUMP(p)) {
-		cl_index n = fixnnint(p);
+		cl_index n = ecl_to_size(p);
 		if (FIXNUMP(x)) {
 			cl_fixnum y = fix(x);
 			if (n >= FIXNUM_BITS) {

commit 6729693650da6c89d049067c66f6461c9d7c6fb0

fix fixint and fixnnint for apis with long smaller than cl_fixnum
Concerns especially win64. These functions have been broken since
commit 0102aed9f57f69b8cb75d518fc8c701cb437b1d8.

src/c/big.d
@@ -14,6 +14,7 @@
  */
 
 #define ECL_INCLUDE_MATH_H
+#include <limits.h>
 #include <string.h>
 #include <ecl/ecl.h>
 #include <ecl/internal.h>
@@ -334,36 +335,6 @@ mp_realloc(void *ptr, size_t osize, size_t nsize)
   return p;
 }
 
-cl_fixnum
-fixint(cl_object x)
-{
-  if (ECL_FIXNUMP(x))
-    return ecl_fixnum(x);
-  if (ECL_BIGNUMP(x)) {
-    if (mpz_fits_slong_p(x->big.big_num)) {
-      return mpz_get_si(x->big.big_num);
-    }
-  }
-  FEwrong_type_argument(@[fixnum], x);
-}
-
-cl_index
-fixnnint(cl_object x)
-{
-  if (ECL_FIXNUMP(x)) {
-    cl_fixnum i = ecl_fixnum(x);
-    if (i >= 0)
-      return i;
-  } else if (ECL_BIGNUMP(x)) {
-    if (mpz_fits_ulong_p(x->big.big_num)) {
-      return mpz_get_ui(x->big.big_num);
-    }
-  }
-  FEwrong_type_argument(cl_list(3, @'integer', ecl_make_fixnum(0),
-                                ecl_make_fixnum(MOST_POSITIVE_FIXNUM)),
-                        x);
-}
-
 #undef _ecl_big_set_fixnum
 #undef _ecl_big_set_index
 #if ECL_LONG_BITS >= ECL_FIXNUM_BITS
@@ -405,6 +376,7 @@ _ecl_big_set_fixnum(cl_object x, cl_fixnum f)
     ECL_BIGNUM_SIZE(x) = -1;
     ECL_BIGNUM_LIMBS(x)[0] = -f;
   }
+  return x;
 }
 
 cl_object
@@ -416,6 +388,7 @@ _ecl_big_set_index(cl_object x, cl_index f)
     ECL_BIGNUM_SIZE(x) = 1;
     ECL_BIGNUM_LIMBS(x)[0] = f;
   }
+  return x;
 }
 
 cl_fixnum
@@ -433,16 +406,82 @@ _ecl_big_get_index(cl_object x)
   cl_index output = ECL_BIGNUM_LIMBS(x)[0];
   return (ECL_BIGNUM_SIZE(x) > 0)? output : ~(output - 1);
 }
-
-bool
-_ecl_big_fits_in_index(cl_object x)
-{
-  /* INV: x is a bignum and thus size != 0 */
-  return (ECL_BIGNUM_SIZE(x) ^ 1) == 0;
-}
 #else
 # error "ECL cannot build with GMP when both long and mp_limb_t are smaller than cl_fixnum"
 #endif /* ECL_FIXNUM_BITS > GMP_LIMB_BITS, ECL_LONG_BITS */
+
+#if ECL_FIXNUM_BITS == ECL_INT_BITS
+static inline bool
+_ecl_big_fits_in_fixnum(cl_object x)
+{
+  return mpz_fits_sint_p(x->big.big_num);
+}
+
+static inline bool
+_ecl_big_fits_in_index(cl_object x)
+{
+  return mpz_fits_uint_p(x->big.big_num);
+}
+#elif ECL_FIXNUM_BITS == ECL_LONG_BITS
+static inline bool
+_ecl_big_fits_in_fixnum(cl_object x)
+{
+  return mpz_fits_slong_p(x->big.big_num);
+}
+
+static inline bool
+_ecl_big_fits_in_index(cl_object x)
+{
+  return mpz_fits_ulong_p(x->big.big_num);
+}
+#elif ECL_FIXNUM_BITS == ECL_LONG_LONG_BITS && GMP_LIMB_BITS >= ECL_FIXNUM_BITS
+static inline bool
+_ecl_big_fits_in_fixnum(cl_object x)
+{
+  /* INV: x is a bignum and thus size != 0 */
+  return (ECL_BIGNUM_SIZE(x) == 1 && ECL_BIGNUM_LIMBS(x)[0] <= LLONG_MAX)
+      || (ECL_BIGNUM_SIZE(x) == -1 && -(ECL_BIGNUM_LIMBS(x)[0]) >= LLONG_MIN);
+}
+
+static inline bool
+_ecl_big_fits_in_index(cl_object x)
+{
+  /* INV: x is a bignum and thus size != 0 */
+  return ECL_BIGNUM_SIZE(x) == 1 && ECL_BIGNUM_LIMBS(x)[0] <= ULLONG_MAX;
+}
+#else
+# error "ECL cannot build with GMP when both long and mp_limb_t are smaller than cl_fixnum"
+#endif
+
+cl_fixnum
+fixint(cl_object x)
+{
+  if (ECL_FIXNUMP(x))
+    return ecl_fixnum(x);
+  if (ECL_BIGNUMP(x)) {
+    if (_ecl_big_fits_in_fixnum(x)) {
+      return _ecl_big_get_fixnum(x);
+    }
+  }
+  FEwrong_type_argument(@[fixnum], x);
+}
+
+cl_index
+fixnnint(cl_object x)
+{
+  if (ECL_FIXNUMP(x)) {
+    cl_fixnum i = ecl_fixnum(x);
+    if (i >= 0)
+      return i;
+  } else if (ECL_BIGNUMP(x)) {
+    if (_ecl_big_fits_in_index(x)) {
+      return _ecl_big_get_index(x);
+    }
+  }
+  FEwrong_type_argument(cl_list(3, @'integer', ecl_make_fixnum(0),
+                                ecl_make_fixnum(MOST_POSITIVE_FIXNUM)),
+                        x);
+}
 
 long double
 _ecl_big_to_long_double(cl_object o)