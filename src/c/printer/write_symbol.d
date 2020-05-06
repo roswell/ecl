roswell/ecl

commit c6b4296bb886ad70b83c5cc0f472f6855783e2f9

cosmetic: fix some compiler warnings

src/c/printer/write_symbol.d
@@ -102,13 +102,15 @@ needs_to_be_escaped(cl_object s, cl_object readtable, cl_object print_case)
   return 0;
 }
 
-#define buffer_write_char(c, buffer, stream, buffer_ndx, buffer_size) \
-  ecl_char_set(buffer, buffer_ndx++, c);                              \
-  if (buffer_ndx >= buffer_size) {                                    \
-    si_fill_pointer_set(buffer, ecl_make_fixnum(buffer_size));        \
-    si_do_write_sequence(buffer, stream, ecl_make_fixnum(0), ECL_NIL);\
-    buffer_ndx = 0;                                                   \
+static inline void
+buffer_write_char(char c, cl_object buffer, cl_object stream, cl_index *buffer_ndx, cl_index buffer_size) {
+  ecl_char_set(buffer, (*buffer_ndx)++, c);
+  if (*buffer_ndx >= buffer_size) {
+    si_fill_pointer_set(buffer, ecl_make_fixnum(buffer_size));
+    si_do_write_sequence(buffer, stream, ecl_make_fixnum(0), ECL_NIL);
+    *buffer_ndx = 0;
   }
+}
 
 static void
 write_symbol_string(cl_object s, int action, cl_object print_case,
@@ -124,13 +126,13 @@ write_symbol_string(cl_object s, int action, cl_object print_case,
   cl_index buffer_size = ecl_fixnum(cl_array_total_size(buffer));
   cl_index buffer_ndx = 0;
   if (escape)
-    buffer_write_char('|', buffer, stream, buffer_ndx, buffer_size);
+    buffer_write_char('|', buffer, stream, &buffer_ndx, buffer_size);
   capitalize = 1;
   for (i = 0;  i < s->base_string.fillp;  i++) {
     ecl_character c = ecl_char(s, i);
     if (escape) {
       if (c == '|' || c == '\\') {
-        buffer_write_char('\\', buffer, stream, buffer_ndx, buffer_size);
+        buffer_write_char('\\', buffer, stream, &buffer_ndx, buffer_size);
       }
     } else if (action != ecl_case_preserve) {
       if (ecl_upper_case_p(c)) {
@@ -155,10 +157,10 @@ write_symbol_string(cl_object s, int action, cl_object print_case,
         capitalize = !ecl_alphanumericp(c);
       }
     }
-    buffer_write_char(c, buffer, stream, buffer_ndx, buffer_size);
+    buffer_write_char(c, buffer, stream, &buffer_ndx, buffer_size);
   }
   if (escape)
-    buffer_write_char('|', buffer, stream, buffer_ndx, buffer_size);
+    buffer_write_char('|', buffer, stream, &buffer_ndx, buffer_size);
   si_fill_pointer_set(buffer, ecl_make_fixnum(buffer_ndx));
   si_do_write_sequence(buffer, stream, ecl_make_fixnum(0), ECL_NIL);
   si_put_buffer_string(buffer);

contrib/bytecmp/bytecmp.lsp
@@ -81,7 +81,7 @@
 (defun bc-compile-file-pathname (name &key (output-file name) (type :fasl)
 				 verbose print c-file h-file data-file
 				 shared-data-file system-p load external-format)
-  (declare (ignore load c-file h-file data-file shared-data-file system-p verbose print))
+  (declare (ignore load c-file h-file data-file shared-data-file system-p verbose print external-format))
   (let ((extension "fasc"))
     (case type
       ((:fasl :fas) (setf extension "fasc"))

src/c/alloc_2.d
@@ -191,11 +191,13 @@ static struct ecl_type_information {
   size_t t;
 } type_info[t_end];
 
+#ifdef GBC_BOEHM_PRECISE
 static void
 error_wrong_tag(cl_type t)
 {
   ecl_internal_error("Collector called with invalid tag number.");
 }
+#endif
 
 cl_index
 ecl_object_byte_size(cl_type t)
@@ -764,6 +766,7 @@ extern void (*GC_push_other_roots)();
 static void (*old_GC_push_other_roots)();
 static void stacks_scanner();
 
+#ifdef GBC_BOEHM_PRECISE
 static cl_index
 to_bitmap(void *x, void *y)
 {
@@ -773,6 +776,7 @@ to_bitmap(void *x, void *y)
   n /= sizeof(void*);
   return 1 << n;
 }
+#endif
 
 void
 init_alloc(void)

src/c/clos/cache.d
@@ -36,6 +36,7 @@ empty_cache(ecl_cache_ptr cache)
 #endif
 }
 
+#ifndef ECL_THREADS
 static void
 clear_one_from_cache(ecl_cache_ptr cache, cl_object target)
 {
@@ -51,8 +52,7 @@ clear_one_from_cache(ecl_cache_ptr cache, cl_object target)
     }
   }
 }
-
-#ifdef ECL_THREADS
+#else
 static void
 clear_list_from_cache(ecl_cache_ptr cache)
 {

src/c/compiler.d
@@ -847,7 +847,7 @@ c_block(cl_env_ptr env, cl_object body, int old_flags) {
   struct cl_compiler_env old_env;
   cl_object name = pop(&body);
   cl_object block_record;
-  cl_index labelz, pc, loc, constants;
+  cl_index labelz, pc, constants;
   int flags;
 
   if (!ECL_SYMBOLP(name))
@@ -858,7 +858,7 @@ c_block(cl_env_ptr env, cl_object body, int old_flags) {
   pc = current_pc(env);
 
   flags = maybe_values_or_reg0(old_flags);
-  loc = c_register_block(env, name);
+  c_register_block(env, name);
   block_record = ECL_CONS_CAR(env->c_env->variables);
   if (Null(name)) {
     asm_op(env, OP_DO);
@@ -1063,7 +1063,7 @@ c_case(cl_env_ptr env, cl_object clause, int flags) {
 
 static int
 c_catch(cl_env_ptr env, cl_object args, int flags) {
-  cl_index labelz, loc;
+  cl_index labelz;
   cl_object old_env;
 
   /* Compile evaluation of tag */
@@ -1071,7 +1071,7 @@ c_catch(cl_env_ptr env, cl_object args, int flags) {
 
   /* Compile binding of tag */
   old_env = env->c_env->variables;
-  loc = c_register_block(env, ecl_make_fixnum(0));
+  c_register_block(env, ecl_make_fixnum(0));
   asm_op(env, OP_CATCH);
 
   /* Compile jump point */
@@ -3039,7 +3039,7 @@ c_default(cl_env_ptr env, cl_object var, cl_object stmt, cl_object flag, cl_obje
 cl_object
 ecl_make_lambda(cl_env_ptr env, cl_object name, cl_object lambda) {
   cl_object reqs, opts, rest, key, keys, auxs, allow_other_keys;
-  cl_object specials, doc, decl, body, output;
+  cl_object specials, decl, body, output;
   cl_index handle;
   struct cl_compiler_env *old_c_env, new_c_env;
 
@@ -3057,7 +3057,7 @@ ecl_make_lambda(cl_env_ptr env, cl_object name, cl_object lambda) {
   keys = env->values[4];
   allow_other_keys = env->values[5];
   auxs = env->values[6];
-  doc  = env->values[7];
+  /* doc  = env->values[7]; unused */;
   specials = env->values[8];
   decl = env->values[9];
   body = env->values[10];

src/c/disassembler.d
@@ -53,13 +53,11 @@ print_oparg_arg(const char *s, cl_fixnum n, cl_object x) {
 static void
 disassemble_lambda(cl_object bytecodes) {
   const cl_env_ptr env = ecl_process_env();
-  cl_object *data;
   cl_opcode *vector;
 
   ecl_bds_bind(env, @'*print-pretty*', ECL_NIL);
 
   /* Print required arguments */
-  data = bytecodes->bytecodes.data->vector.self.t;
   cl_print(1,bytecodes->bytecodes.data);
 
   /* Name of LAMBDA */

src/c/ffi/mmap.d
@@ -53,7 +53,7 @@
                        @':element-type', element_type,
                        @':if-exists', if_exists,
                        @':if-does-not-exist', if_does_not_exist,
-                       @':external-format', @':default',
+                       @':external-format', external_format,
                        @':cstream', ECL_NIL);
       fd = ecl_to_int(si_file_stream_fd(stream));
       if (Null(length))

src/c/interpreter.d
@@ -314,7 +314,6 @@ ecl_interpret(cl_object frame, cl_object env, cl_object bytecodes)
   frame_aux.stack = frame_aux.base = 0;
   frame_aux.size = 0;
   frame_aux.env = the_env;
- BEGIN:
   BEGIN_SWITCH {
     CASE(OP_NOP); {
       reg0 = ECL_NIL;

commit e72ef09e78eb1944cc7e47d561cebed43e228d65

printer: fix printing of symbols with non-ascii names
Bug was introduced in commit c6b4296bb886ad70b83c5cc0f472f6855783e2f9
in converting buffer_write_char from a macro to an inline
function. Problem reported by Vladimir Sedach on the ecl-devel mailing
list.

src/c/printer/write_symbol.d
@@ -103,7 +103,7 @@ needs_to_be_escaped(cl_object s, cl_object readtable, cl_object print_case)
 }
 
 static inline void
-buffer_write_char(char c, cl_object buffer, cl_object stream, cl_index *buffer_ndx, cl_index buffer_size) {
+buffer_write_char(ecl_character c, cl_object buffer, cl_object stream, cl_index *buffer_ndx, cl_index buffer_size) {
   ecl_char_set(buffer, (*buffer_ndx)++, c);
   if (*buffer_ndx >= buffer_size) {
     si_fill_pointer_set(buffer, ecl_make_fixnum(buffer_size));