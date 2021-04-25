ecd86a2541161dfe41f166f2ab395ed9afecb0fb
Marius Gerbershagen <marius.gerbershagen@gmail.com>
2021-04-25 20:37:42 +0200

    unicode: update unicode character database to version 13.0.0

--- src/c/unicode/ucd16.c
+++ src/c/unicode/ucd16.c
@@ -1,169 +1,179 @@
-/* -*- Mode: C; c-basic-offset: 8; indent-tabs-mode: nil -*- */
-/* vim: set filetype=c tabstop=8 shiftwidth=4 expandtab: */
+/*
+ * Unicode character database.
+ *
+ * auto-generated, do not edit! (see contrib/unicode/)
+ */
+extern const unsigned char ecl_ucd_page_table_0[];
+extern const unsigned char ecl_ucd_page_table_1[];
+extern const unsigned char ecl_ucd_page_table_2[];
+extern const unsigned char ecl_ucd_page_table_3[];
+extern const unsigned char ecl_ucd_page_table_4[];
+extern const unsigned char ecl_ucd_page_table_5[];
+extern const unsigned char ecl_ucd_page_table_6[];
+extern const unsigned char ecl_ucd_page_table_7[];
+extern const unsigned char ecl_ucd_page_table_8[];
+extern const unsigned char ecl_ucd_page_table_9[];
+extern const unsigned char ecl_ucd_page_table_10[];
+extern const unsigned char ecl_ucd_page_table_11[];
+extern const unsigned char ecl_ucd_page_table_12[];
+extern const unsigned char ecl_ucd_page_table_13[];
+extern const unsigned char ecl_ucd_page_table_14[];
+extern const unsigned char ecl_ucd_page_table_15[];
+extern const unsigned char ecl_ucd_page_table_16[];
+extern const unsigned char ecl_ucd_page_table_17[];
+extern const unsigned char ecl_ucd_page_table_18[];
+extern const unsigned char ecl_ucd_page_table_19[];
+extern const unsigned char ecl_ucd_page_table_20[];
+extern const unsigned char ecl_ucd_page_table_21[];
+extern const unsigned char ecl_ucd_page_table_22[];
+extern const unsigned char ecl_ucd_page_table_23[];
+extern const unsigned char ecl_ucd_page_table_24[];
+extern const unsigned char ecl_ucd_page_table_25[];
+extern const unsigned char ecl_ucd_page_table_26[];
+extern const unsigned char ecl_ucd_page_table_27[];
+extern const unsigned char ecl_ucd_page_table_28[];
+extern const unsigned char ecl_ucd_page_table_29[];
+extern const unsigned char ecl_ucd_page_table_30[];
+extern const unsigned char ecl_ucd_page_table_31[];
+extern const unsigned char ecl_ucd_page_table_32[];
+extern const unsigned char ecl_ucd_page_table_33[];
+extern const unsigned char ecl_ucd_page_table_34[];
+extern const unsigned char ecl_ucd_page_table_35[];
+extern const unsigned char ecl_ucd_page_table_36[];
+extern const unsigned char ecl_ucd_page_table_37[];
+extern const unsigned char ecl_ucd_page_table_38[];
+extern const unsigned char ecl_ucd_page_table_39[];
+extern const unsigned char ecl_ucd_page_table_40[];
+extern const unsigned char ecl_ucd_page_table_41[];
+extern const unsigned char ecl_ucd_page_table_42[];
+extern const unsigned char ecl_ucd_page_table_43[];
+extern const unsigned char ecl_ucd_page_table_44[];
+extern const unsigned char ecl_ucd_page_table_45[];
+extern const unsigned char ecl_ucd_page_table_46[];
+extern const unsigned char ecl_ucd_page_table_47[];
+extern const unsigned char ecl_ucd_page_table_48[];
+extern const unsigned char ecl_ucd_page_table_49[];
+extern const unsigned char ecl_ucd_page_table_50[];
+extern const unsigned char ecl_ucd_page_table_51[];
+extern const unsigned char ecl_ucd_page_table_52[];
+extern const unsigned char ecl_ucd_page_table_53[];
+extern const unsigned char ecl_ucd_page_table_54[];
+extern const unsigned char ecl_ucd_page_table_55[];
+extern const unsigned char ecl_ucd_page_table_56[];
+extern const unsigned char ecl_ucd_page_table_57[];
+extern const unsigned char ecl_ucd_page_table_58[];
+extern const unsigned char ecl_ucd_page_table_59[];
+extern const unsigned char ecl_ucd_page_table_60[];
+extern const unsigned char ecl_ucd_page_table_61[];
+extern const unsigned char ecl_ucd_page_table_62[];
+extern const unsigned char ecl_ucd_page_table_63[];
+extern const unsigned char ecl_ucd_page_table_64[];
+extern const unsigned char ecl_ucd_page_table_65[];
+extern const unsigned char ecl_ucd_page_table_66[];
+extern const unsigned char ecl_ucd_page_table_67[];
+extern const unsigned char ecl_ucd_page_table_68[];
+extern const unsigned char ecl_ucd_page_table_69[];
+extern const unsigned char ecl_ucd_page_table_70[];
 
-
-extern const char ecl_ucd_page_table_0[];
-extern const char ecl_ucd_page_table_1[];
-extern const char ecl_ucd_page_table_2[];
-extern const char ecl_ucd_page_table_3[];
-extern const char ecl_ucd_page_table_4[];
-extern const char ecl_ucd_page_table_5[];
-extern const char ecl_ucd_page_table_6[];
-extern const char ecl_ucd_page_table_7[];
-extern const char ecl_ucd_page_table_8[];
-extern const char ecl_ucd_page_table_9[];
-extern const char ecl_ucd_page_table_10[];
-extern const char ecl_ucd_page_table_11[];
-extern const char ecl_ucd_page_table_12[];
-extern const char ecl_ucd_page_table_13[];
-extern const char ecl_ucd_page_table_14[];
-extern const char ecl_ucd_page_table_15[];
-extern const char ecl_ucd_page_table_16[];
-extern const char ecl_ucd_page_table_17[];
-extern const char ecl_ucd_page_table_18[];
-extern const char ecl_ucd_page_table_19[];
-extern const char ecl_ucd_page_table_20[];
-extern const char ecl_ucd_page_table_21[];
-extern const char ecl_ucd_page_table_22[];
-extern const char ecl_ucd_page_table_23[];
-extern const char ecl_ucd_page_table_24[];
-extern const char ecl_ucd_page_table_25[];
-extern const char ecl_ucd_page_table_26[];
-extern const char ecl_ucd_page_table_27[];
-extern const char ecl_ucd_page_table_28[];
-extern const char ecl_ucd_page_table_29[];
-extern const char ecl_ucd_page_table_30[];
-extern const char ecl_ucd_page_table_31[];
-extern const char ecl_ucd_page_table_32[];
-extern const char ecl_ucd_page_table_33[];
-extern const char ecl_ucd_page_table_34[];
-extern const char ecl_ucd_page_table_35[];
-extern const char ecl_ucd_page_table_36[];
-extern const char ecl_ucd_page_table_37[];
-extern const char ecl_ucd_page_table_38[];
-extern const char ecl_ucd_page_table_39[];
-extern const char ecl_ucd_page_table_40[];
-extern const char ecl_ucd_page_table_41[];
-extern const char ecl_ucd_page_table_42[];
-extern const char ecl_ucd_page_table_43[];
-extern const char ecl_ucd_page_table_44[];
-extern const char ecl_ucd_page_table_45[];
-extern const char ecl_ucd_page_table_46[];
-extern const char ecl_ucd_page_table_47[];
-extern const char ecl_ucd_page_table_48[];
-extern const char ecl_ucd_page_table_49[];
-extern const char ecl_ucd_page_table_50[];
-extern const char ecl_ucd_page_table_51[];
-extern const char ecl_ucd_page_table_52[];
-extern const char ecl_ucd_page_table_53[];
-extern const char ecl_ucd_page_table_54[];
-extern const char ecl_ucd_page_table_55[];
-extern const char ecl_ucd_page_table_56[];
-extern const char ecl_ucd_page_table_57[];
-extern const char ecl_ucd_page_table_58[];
-extern const char ecl_ucd_page_table_59[];
-extern const char ecl_ucd_page_table_60[];
-extern const char ecl_ucd_page_table_61[];
-extern const char ecl_ucd_page_table_62[];
-extern const char ecl_ucd_page_table_63[];
-extern const char ecl_ucd_page_table_64[];
-extern const char ecl_ucd_page_table_65[];
-extern const char ecl_ucd_page_table_66[];
-extern const char ecl_ucd_page_table_67[];
-extern const char ecl_ucd_page_table_68[];
-extern const char ecl_ucd_page_table_69[];
-extern const char ecl_ucd_page_table_70[];
-
-const unsigned char ecl_ucd_misc_table[1736] = {
-0, 8, 0, 255, 255, 0, 0, 0
-,1, 8, 0, 255, 255, 0, 0, 0
+const unsigned char ecl_ucd_misc_table[1840] = {
+0, 9, 0, 255, 255, 0, 0, 0
+,0, 17, 0, 255, 255, 0, 0, 0
+,1, 9, 0, 255, 255, 0, 0, 0
+,1, 17, 0, 255, 255, 0, 0, 0
 ,0, 0, 0, 255, 255, 0, 0, 0
-,0, 8, 0, 255, 255, 0, 0, 0
-,1, 8, 0, 255, 255, 0, 0, 0
-,2, 8, 0, 255, 255, 0, 0, 0
+,0, 9, 0, 255, 255, 0, 0, 0
+,1, 9, 0, 255, 255, 0, 0, 0
+,2, 9, 0, 255, 255, 0, 0, 0
 ,3, 0, 0, 255, 255, 0, 0, 0
-,3, 8, 0, 255, 255, 0, 0, 0
-,3, 12, 0, 255, 255, 0, 0, 0
+,3, 9, 0, 255, 255, 0, 0, 0
 ,3, 14, 0, 255, 255, 0, 0, 0
+,3, 17, 0, 255, 255, 0, 0, 0
 ,4, 0, 0, 255, 255, 0, 0, 0
-,4, 8, 0, 255, 255, 0, 0, 0
-,4, 14, 0, 255, 255, 0, 0, 0
+,4, 9, 0, 255, 255, 0, 0, 0
+,4, 17, 0, 255, 255, 0, 0, 0
 ,5, 2, 0, 255, 255, 0, 0, 0
 ,5, 3, 0, 255, 255, 0, 0, 0
-,5, 17, 0, 255, 255, 0, 0, 0
-,5, 18, 0, 255, 255, 0, 0, 0
+,5, 21, 0, 255, 255, 0, 0, 0
+,5, 22, 0, 255, 255, 0, 0, 0
+,6, 0, 0, 255, 255, 0, 0, 0
 ,6, 1, 0, 255, 255, 0, 0, 0
 ,6, 3, 0, 255, 255, 0, 0, 0
 ,6, 8, 0, 255, 255, 0, 0, 0
 ,6, 9, 0, 255, 255, 0, 0, 0
 ,6, 10, 0, 255, 255, 0, 0, 0
+,6, 11, 0, 255, 255, 0, 0, 0
 ,6, 12, 0, 255, 255, 0, 0, 0
-,6, 13, 0, 255, 255, 0, 0, 0
 ,6, 14, 0, 255, 255, 0, 0, 0
 ,6, 15, 0, 255, 255, 0, 0, 0
 ,6, 16, 0, 255, 255, 0, 0, 0
-,7, 8, 0, 255, 255, 0, 0, 0
-,8, 8, 0, 255, 255, 0, 0, 0
-,9, 8, 0, 255, 255, 0, 0, 0
-,9, 8, 9, 255, 255, 0, 0, 0
-,9, 8, 216, 255, 255, 0, 0, 0
-,9, 8, 226, 255, 255, 0, 0, 0
-,10, 11, 0, 255, 255, 0, 0, 0
-,11, 8, 0, 255, 255, 0, 0, 0
-,11, 11, 0, 255, 255, 0, 0, 0
-,11, 11, 1, 255, 255, 0, 0, 0
-,11, 11, 7, 255, 255, 0, 0, 0
-,11, 11, 8, 255, 255, 0, 0, 0
-,11, 11, 9, 255, 255, 0, 0, 0
-,11, 11, 10, 255, 255, 0, 0, 0
-,11, 11, 11, 255, 255, 0, 0, 0
-,11, 11, 12, 255, 255, 0, 0, 0
-,11, 11, 13, 255, 255, 0, 0, 0
-,11, 11, 14, 255, 255, 0, 0, 0
-,11, 11, 15, 255, 255, 0, 0, 0
-,11, 11, 16, 255, 255, 0, 0, 0
-,11, 11, 17, 255, 255, 0, 0, 0
-,11, 11, 18, 255, 255, 0, 0, 0
-,11, 11, 19, 255, 255, 0, 0, 0
-,11, 11, 20, 255, 255, 0, 0, 0
-,11, 11, 21, 255, 255, 0, 0, 0
-,11, 11, 22, 255, 255, 0, 0, 0
-,11, 11, 23, 255, 255, 0, 0, 0
-,11, 11, 24, 255, 255, 0, 0, 0
-,11, 11, 25, 255, 255, 0, 0, 0
-,11, 11, 26, 255, 255, 0, 0, 0
-,11, 11, 27, 255, 255, 0, 0, 0
-,11, 11, 28, 255, 255, 0, 0, 0
-,11, 11, 29, 255, 255, 0, 0, 0
-,11, 11, 30, 255, 255, 0, 0, 0
-,11, 11, 31, 255, 255, 0, 0, 0
-,11, 11, 32, 255, 255, 0, 0, 0
-,11, 11, 33, 255, 255, 0, 0, 0
-,11, 11, 34, 255, 255, 0, 0, 0
-,11, 11, 35, 255, 255, 0, 0, 0
-,11, 11, 36, 255, 255, 0, 0, 0
-,11, 11, 84, 255, 255, 0, 0, 0
-,11, 11, 91, 255, 255, 0, 0, 0
-,11, 11, 103, 255, 255, 0, 0, 0
-,11, 11, 107, 255, 255, 0, 0, 0
-,11, 11, 118, 255, 255, 0, 0, 0
-,11, 11, 122, 255, 255, 0, 0, 0
-,11, 11, 129, 255, 255, 0, 0, 0
-,11, 11, 130, 255, 255, 0, 0, 0
-,11, 11, 132, 255, 255, 0, 0, 0
-,11, 11, 202, 255, 255, 0, 0, 0
-,11, 11, 214, 255, 255, 0, 0, 0
-,11, 11, 216, 255, 255, 0, 0, 0
-,11, 11, 218, 255, 255, 0, 0, 0
-,11, 11, 220, 255, 255, 0, 0, 0
-,11, 11, 222, 255, 255, 0, 0, 0
-,11, 11, 224, 255, 255, 0, 0, 0
-,11, 11, 228, 255, 255, 0, 0, 0
-,11, 11, 230, 255, 255, 0, 0, 0
-,11, 11, 232, 255, 255, 0, 0, 0
-,11, 11, 233, 255, 255, 0, 0, 0
-,11, 11, 234, 255, 255, 0, 0, 0
-,11, 11, 240, 255, 255, 0, 0, 0
+,6, 17, 0, 255, 255, 0, 0, 0
+,6, 18, 0, 255, 255, 0, 0, 0
+,6, 19, 0, 255, 255, 0, 0, 0
+,6, 20, 0, 255, 255, 0, 0, 0
+,7, 9, 0, 255, 255, 0, 0, 0
+,8, 9, 0, 255, 255, 0, 0, 0
+,9, 9, 0, 255, 255, 0, 0, 0
+,9, 9, 6, 255, 255, 0, 0, 0
+,9, 9, 9, 255, 255, 0, 0, 0
+,9, 9, 216, 255, 255, 0, 0, 0
+,9, 9, 224, 255, 255, 0, 0, 0
+,9, 9, 226, 255, 255, 0, 0, 0
+,10, 13, 0, 255, 255, 0, 0, 0
+,11, 9, 0, 255, 255, 0, 0, 0
+,11, 9, 9, 255, 255, 0, 0, 0
+,11, 13, 0, 255, 255, 0, 0, 0
+,11, 13, 1, 255, 255, 0, 0, 0
+,11, 13, 7, 255, 255, 0, 0, 0
+,11, 13, 8, 255, 255, 0, 0, 0
+,11, 13, 9, 255, 255, 0, 0, 0
+,11, 13, 10, 255, 255, 0, 0, 0
+,11, 13, 11, 255, 255, 0, 0, 0
+,11, 13, 12, 255, 255, 0, 0, 0
+,11, 13, 13, 255, 255, 0, 0, 0
+,11, 13, 14, 255, 255, 0, 0, 0
+,11, 13, 15, 255, 255, 0, 0, 0
+,11, 13, 16, 255, 255, 0, 0, 0
+,11, 13, 17, 255, 255, 0, 0, 0
+,11, 13, 18, 255, 255, 0, 0, 0
+,11, 13, 19, 255, 255, 0, 0, 0
+,11, 13, 20, 255, 255, 0, 0, 0
+,11, 13, 21, 255, 255, 0, 0, 0
+,11, 13, 22, 255, 255, 0, 0, 0
+,11, 13, 23, 255, 255, 0, 0, 0
+,11, 13, 24, 255, 255, 0, 0, 0
+,11, 13, 25, 255, 255, 0, 0, 0
+,11, 13, 26, 255, 255, 0, 0, 0
+,11, 13, 27, 255, 255, 0, 0, 0
+,11, 13, 28, 255, 255, 0, 0, 0
+,11, 13, 29, 255, 255, 0, 0, 0
+,11, 13, 30, 255, 255, 0, 0, 0
+,11, 13, 31, 255, 255, 0, 0, 0
+,11, 13, 32, 255, 255, 0, 0, 0
+,11, 13, 33, 255, 255, 0, 0, 0
+,11, 13, 34, 255, 255, 0, 0, 0
+,11, 13, 35, 255, 255, 0, 0, 0
+,11, 13, 36, 255, 255, 0, 0, 0
+,11, 13, 84, 255, 255, 0, 0, 0
+,11, 13, 91, 255, 255, 0, 0, 0
+,11, 13, 103, 255, 255, 0, 0, 0
+,11, 13, 107, 255, 255, 0, 0, 0
+,11, 13, 118, 255, 255, 0, 0, 0
+,11, 13, 122, 255, 255, 0, 0, 0
+,11, 13, 129, 255, 255, 0, 0, 0
+,11, 13, 130, 255, 255, 0, 0, 0
+,11, 13, 132, 255, 255, 0, 0, 0
+,11, 13, 202, 255, 255, 0, 0, 0
+,11, 13, 214, 255, 255, 0, 0, 0
+,11, 13, 216, 255, 255, 0, 0, 0
+,11, 13, 218, 255, 255, 0, 0, 0
+,11, 13, 220, 255, 255, 0, 0, 0
+,11, 13, 222, 255, 255, 0, 0, 0
+,11, 13, 228, 255, 255, 0, 0, 0
+,11, 13, 230, 255, 255, 0, 0, 0
+,11, 13, 232, 255, 255, 0, 0, 0
+,11, 13, 233, 255, 255, 0, 0, 0
+,11, 13, 234, 255, 255, 0, 0, 0
+,11, 13, 240, 255, 255, 0, 0, 0
 ,12, 1, 0, 0, 0, 0, 0, 0
 ,12, 1, 0, 1, 1, 0, 0, 0
 ,12, 1, 0, 2, 2, 0, 0, 0
@@ -184,28 +194,29 @@ const unsigned char ecl_ucd_misc_table[1736] = {
 ,12, 5, 0, 7, 7, 0, 0, 0
 ,12, 5, 0, 8, 8, 0, 0, 0
 ,12, 5, 0, 9, 9, 0, 0, 0
-,12, 8, 0, 0, 0, 0, 0, 0
-,12, 8, 0, 1, 1, 0, 0, 0
-,12, 8, 0, 2, 2, 0, 0, 0
-,12, 8, 0, 3, 3, 0, 0, 0
-,12, 8, 0, 4, 4, 0, 0, 0
-,12, 8, 0, 5, 5, 0, 0, 0
-,12, 8, 0, 6, 6, 0, 0, 0
-,12, 8, 0, 7, 7, 0, 0, 0
-,12, 8, 0, 8, 8, 0, 0, 0
-,12, 8, 0, 9, 9, 0, 0, 0
-,12, 14, 0, 0, 0, 0, 0, 0
-,12, 14, 0, 1, 1, 0, 0, 0
-,12, 14, 0, 2, 2, 0, 0, 0
-,12, 14, 0, 3, 3, 0, 0, 0
-,12, 14, 0, 4, 4, 0, 0, 0
-,12, 14, 0, 5, 5, 0, 0, 0
-,12, 14, 0, 6, 6, 0, 0, 0
-,12, 14, 0, 7, 7, 0, 0, 0
-,12, 14, 0, 8, 8, 0, 0, 0
-,12, 14, 0, 9, 9, 0, 0, 0
-,13, 8, 0, 255, 255, 0, 0, 0
-,13, 12, 0, 255, 255, 0, 0, 0
+,12, 9, 0, 0, 0, 0, 0, 0
+,12, 9, 0, 1, 1, 0, 0, 0
+,12, 9, 0, 2, 2, 0, 0, 0
+,12, 9, 0, 3, 3, 0, 0, 0
+,12, 9, 0, 4, 4, 0, 0, 0
+,12, 9, 0, 5, 5, 0, 0, 0
+,12, 9, 0, 6, 6, 0, 0, 0
+,12, 9, 0, 7, 7, 0, 0, 0
+,12, 9, 0, 8, 8, 0, 0, 0
+,12, 9, 0, 9, 9, 0, 0, 0
+,12, 17, 0, 0, 0, 0, 0, 0
+,12, 17, 0, 1, 1, 0, 0, 0
+,12, 17, 0, 2, 2, 0, 0, 0
+,12, 17, 0, 3, 3, 0, 0, 0
+,12, 17, 0, 4, 4, 0, 0, 0
+,12, 17, 0, 5, 5, 0, 0, 0
+,12, 17, 0, 6, 6, 0, 0, 0
+,12, 17, 0, 7, 7, 0, 0, 0
+,12, 17, 0, 8, 8, 0, 0, 0
+,12, 17, 0, 9, 9, 0, 0, 0
+,13, 9, 0, 255, 255, 0, 0, 0
+,13, 14, 0, 255, 255, 0, 0, 0
+,14, 0, 0, 255, 255, 0, 0, 0
 ,14, 1, 0, 255, 255, 0, 0, 0
 ,14, 1, 0, 255, 1, 0, 0, 0
 ,14, 1, 0, 255, 2, 0, 0, 0
@@ -227,71 +238,74 @@ const unsigned char ecl_ucd_misc_table[1736] = {
 ,14, 5, 0, 255, 7, 0, 0, 0
 ,14, 5, 0, 255, 8, 0, 0, 0
 ,14, 5, 0, 255, 9, 0, 0, 0
-,14, 8, 0, 255, 255, 0, 0, 0
-,14, 8, 0, 255, 1, 0, 0, 0
-,14, 8, 0, 255, 2, 0, 0, 0
-,14, 8, 0, 255, 3, 0, 0, 0
-,14, 8, 0, 255, 4, 0, 0, 0
-,14, 8, 0, 255, 5, 0, 0, 0
-,14, 8, 0, 255, 6, 0, 0, 0
-,14, 8, 0, 255, 7, 0, 0, 0
-,14, 8, 0, 255, 8, 0, 0, 0
-,14, 8, 0, 255, 9, 0, 0, 0
-,14, 12, 0, 255, 255, 0, 0, 0
-,14, 12, 0, 255, 0, 0, 0, 0
-,14, 12, 0, 255, 1, 0, 0, 0
-,14, 12, 0, 255, 2, 0, 0, 0
-,14, 12, 0, 255, 3, 0, 0, 0
-,14, 12, 0, 255, 4, 0, 0, 0
-,14, 12, 0, 255, 5, 0, 0, 0
-,14, 12, 0, 255, 6, 0, 0, 0
-,14, 12, 0, 255, 7, 0, 0, 0
-,14, 12, 0, 255, 8, 0, 0, 0
-,14, 12, 0, 255, 9, 0, 0, 0
+,14, 9, 0, 255, 255, 0, 0, 0
+,14, 9, 0, 255, 1, 0, 0, 0
+,14, 9, 0, 255, 2, 0, 0, 0
+,14, 9, 0, 255, 3, 0, 0, 0
+,14, 9, 0, 255, 4, 0, 0, 0
+,14, 9, 0, 255, 5, 0, 0, 0
+,14, 9, 0, 255, 6, 0, 0, 0
+,14, 9, 0, 255, 7, 0, 0, 0
+,14, 9, 0, 255, 8, 0, 0, 0
+,14, 9, 0, 255, 9, 0, 0, 0
 ,14, 14, 0, 255, 255, 0, 0, 0
+,14, 14, 0, 255, 0, 0, 0, 0
 ,14, 14, 0, 255, 1, 0, 0, 0
 ,14, 14, 0, 255, 2, 0, 0, 0
 ,14, 14, 0, 255, 3, 0, 0, 0
 ,14, 14, 0, 255, 4, 0, 0, 0
-,15, 12, 0, 255, 255, 0, 0, 0
+,14, 14, 0, 255, 5, 0, 0, 0
+,14, 14, 0, 255, 6, 0, 0, 0
+,14, 14, 0, 255, 7, 0, 0, 0
+,14, 14, 0, 255, 8, 0, 0, 0
+,14, 14, 0, 255, 9, 0, 0, 0
+,14, 17, 0, 255, 255, 0, 0, 0
+,14, 17, 0, 255, 1, 0, 0, 0
+,14, 17, 0, 255, 2, 0, 0, 0
+,14, 17, 0, 255, 3, 0, 0, 0
+,14, 17, 0, 255, 4, 0, 0, 0
+,15, 14, 0, 255, 255, 0, 0, 0
 ,16, 6, 0, 255, 255, 0, 0, 0
-,16, 12, 0, 255, 255, 0, 0, 0
 ,16, 14, 0, 255, 255, 0, 0, 0
-,17, 12, 0, 255, 255, 0, 0, 0
-,17, 12, 0, 255, 255, 1, 0, 0
-,18, 12, 0, 255, 255, 0, 0, 0
-,18, 12, 0, 255, 255, 1, 0, 0
-,19, 12, 0, 255, 255, 0, 0, 0
-,19, 12, 0, 255, 255, 1, 0, 0
+,16, 17, 0, 255, 255, 0, 0, 0
+,17, 14, 0, 255, 255, 0, 0, 0
+,17, 14, 0, 255, 255, 1, 0, 0
+,18, 14, 0, 255, 255, 0, 0, 0
+,18, 14, 0, 255, 255, 1, 0, 0
+,19, 14, 0, 255, 255, 0, 0, 0
+,19, 14, 0, 255, 255, 1, 0, 0
 ,20, 0, 0, 255, 255, 0, 0, 0
 ,20, 1, 0, 255, 255, 0, 0, 0
 ,20, 4, 0, 255, 255, 0, 0, 0
 ,20, 7, 0, 255, 255, 0, 0, 0
-,20, 8, 0, 255, 255, 0, 0, 0
-,20, 12, 0, 255, 255, 0, 0, 0
+,20, 9, 0, 255, 255, 0, 0, 0
 ,20, 14, 0, 255, 255, 0, 0, 0
-,21, 12, 0, 255, 255, 0, 0, 0
-,21, 12, 0, 255, 255, 1, 0, 0
+,20, 17, 0, 255, 255, 0, 0, 0
+,21, 14, 0, 255, 255, 0, 0, 0
+,21, 14, 0, 255, 255, 1, 0, 0
 ,22, 0, 0, 255, 255, 0, 0, 0
 ,22, 7, 0, 255, 255, 0, 0, 0
+,22, 17, 0, 255, 255, 0, 0, 0
 ,23, 0, 0, 255, 255, 0, 0, 0
-,23, 8, 0, 255, 255, 0, 0, 0
-,23, 12, 0, 255, 255, 0, 0, 0
+,23, 9, 0, 255, 255, 0, 0, 0
+,23, 14, 0, 255, 255, 0, 0, 0
 ,24, 0, 0, 255, 255, 0, 0, 0
 ,24, 4, 0, 255, 255, 0, 0, 0
 ,24, 6, 0, 255, 255, 0, 0, 0
 ,24, 7, 0, 255, 255, 0, 0, 0
-,24, 8, 0, 255, 255, 0, 0, 0
-,24, 12, 0, 255, 255, 0, 0, 0
-,24, 12, 0, 255, 255, 1, 0, 0
+,24, 9, 0, 255, 255, 0, 0, 0
+,24, 14, 0, 255, 255, 0, 0, 0
+,24, 14, 0, 255, 255, 1, 0, 0
 ,25, 0, 0, 255, 255, 0, 0, 0
 ,25, 7, 0, 255, 255, 0, 0, 0
-,25, 8, 0, 255, 255, 0, 0, 0
-,25, 12, 0, 255, 255, 0, 0, 0
-,26, 18, 0, 255, 255, 0, 0, 0
+,25, 9, 0, 255, 255, 0, 0, 0
+,25, 14, 0, 255, 255, 0, 0, 0
+,25, 14, 0, 255, 255, 1, 0, 0
+,25, 17, 0, 255, 255, 0, 0, 0
+,26, 22, 0, 255, 255, 0, 0, 0
 ,27, 2, 0, 255, 255, 0, 0, 0
 ,28, 4, 0, 255, 255, 0, 0, 0
-,28, 18, 0, 255, 255, 0, 0, 0
+,28, 22, 0, 255, 255, 0, 0, 0
 };
 
 const unsigned char *const ecl_ucd_page_table[256] = {
