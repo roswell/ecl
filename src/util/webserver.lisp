63ff85ef7f80e2a2fc6ac02380423bb75ca048f7
Daniel Kochmański <daniel@turtleware.eu>
2022-11-11 11:56:34 +0100

    install: add build instructions for emscripten

---
+++ src/util/webserver.lisp
@@ -0,0 +1,15 @@
+;;; bsd-2-clause, (c) 2022, Daniel Kochmański
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload "hunchentoot"))
+
+(defpackage #:webserver
+  (:use #:cl #:hunchentoot))
+(in-package #:webserver)
+
+(defvar *acceptor*
+  (make-instance 'hunchentoot:easy-acceptor :port 8888))
+
+(print `(serving ,(ext:getcwd)))
+(push (create-folder-dispatcher-and-handler "/" (uiop/os:getcwd)) *dispatch-table*)
+(start *acceptor*)
