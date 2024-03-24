roswell/ecl

commit 2304da9a830a6a00272bd086c11fc55980018d1b

cmpc: separate the data segment creation passes

src/cmp/cmpbackend-cxx/cmppass2-data.lsp
@@ -16,6 +16,30 @@
 
 (in-package "COMPILER")
 
+(defun data-permanent-storage-size ()
+  (length *permanent-objects*))
+
+(defun data-temporary-storage-size ()
+  (length *temporary-objects*))
+
+(defun data-size ()
+  (+ (data-permanent-storage-size)
+     (data-temporary-storage-size)))
+
+(defun data-get-all-objects ()
+  ;; We collect all objects that are to be externalized, but filter out
+  ;; those which will be created by a lisp form.
+  (loop for array in (list *permanent-objects* *temporary-objects*)
+        nconc (loop for vv-record across array
+                    for object = (vv-value vv-record)
+                    collect (cond ((gethash object *load-objects*)
+                                   0)
+                                  ((vv-used-p vv-record)
+                                   object)
+                                  (t
+                                   ;; Value optimized away or not used
+                                   0)))))
+
 (defun data-dump-array ()
   (cond (si:*compiler-constants*
          (setf si:*compiler-constants* (concatenate 'vector (data-get-all-objects)))
@@ -218,6 +242,8 @@
   ;; FIXME! The MSVC compiler does not allow static initialization of bit
   ;; fields. SSE uses always unboxed static constants. No reference is kept to
   ;; them -- it is thus safe to use them even on code that might be unloaded.
+  ;;
+  ;; NOTE *use-static-constants-p* is T only when :ecl-min is a feature.
   (unless (or #+msvc t
               si:*compiler-constants*
               (and (not *use-static-constants-p*)
@@ -227,9 +253,10 @@
     (ext:if-let ((record (find object *static-constants* :key #'first :test #'equal)))
       (second record)
       (ext:when-let ((builder (static-constant-expression object)))
-        (let ((c-name (format nil "_ecl_static_~D" (length *static-constants*))))
-          (push (list object c-name builder) *static-constants*)
-          (make-vv :location c-name :value object))))))
+        (let* ((c-name (format nil "_ecl_static_~D" (length *static-constants*)))
+               (vv (make-vv :location c-name :value object)))
+          (push (list object vv builder) *static-constants*)
+          vv)))))
 
 
 ;;; Inlineable constants
@@ -256,6 +283,72 @@
     (when (constant-variable-p name)
       (ext:when-let ((x (assoc name *optimizable-constants*)))
         (c1form-arg 0 (cdr x))))))
+
+(defun try-inline-core-sym (object)
+  (when (symbolp object)
+    (multiple-value-bind (foundp cname)
+        (si:mangle-name object)
+      (when foundp
+        (make-vv :value object :location cname)))))
+
+
+
+(defun data-empty-loc* ()
+  (insert-vv (make-vv :value *empty-loc* :permanent-p t :used-p t)))
+
+;;; VV is emitted depending on the location and the representation type.
+(defun update-vv (target source)
+  (assert (eql (vv-value target) (vv-value source)))
+  (setf (vv-location target) (vv-location source)
+        (vv-rep-type target) (vv-rep-type source)))
+
+(defun insert-vv (vv)
+  (let* ((arr (if (vv-permanent-p vv)
+                  *permanent-objects*
+                  *temporary-objects*))
+         (ndx (vector-push-extend vv arr)))
+    (setf (vv-location vv) ndx)
+    vv))
+
+(defun search-vv (object &key permanent (errorp t))
+  (let* ((test (if si:*compiler-constants* 'eq 'equal-with-circularity))
+         (item (if permanent
+                   (or (find object *permanent-objects* :test test :key #'vv-value)
+                       (find object *temporary-objects* :test test :key #'vv-value)))))
+    (when (and (null item) errorp)
+      (cmperr "Unable to find object ~s." object))
+    item))
+
+(defun get-object (object &key (permanent t) (errorp t))
+  (or (search-vv object :permanent permanent :errorp nil)
+      (try-inline-core-sym object)
+      (and errorp
+           (cmperr "Unable to find object ~s." object))))
+
+(defun optimize-cxx-data (objects)
+  (flet ((optimize-vv (object)
+           (let ((value (vv-value object)))
+             (ext:if-let ((vv (and (not (vv-always object))
+                                   (or (add-static-constant value)
+                                       (try-inline-core-sym value)))))
+               (update-vv object vv)
+               (insert-vv object)))))
+    (map nil #'optimize-vv objects)))
+
+(defun add-keywords (keywords)
+  ;; We have to build, in the vector VV[], a sequence with all the keywords
+  ;; that this function uses. It does not matter whether each keyword has
+  ;; appeared separately before, because cl_parse_key() needs the whole
+  ;; sequence. However, we can reuse keywords sequences from other functions
+  ;; when they coincide with ours. See C2LAMBDA-EXPR.
+  (ext:if-let ((x (search keywords *permanent-objects*
+                          :test #'(lambda (k record)
+                                    (and (vv-always record)
+                                         (eq k (vv-value record)))))))
+    (elt *permanent-objects* x)
+    (prog1 (insert-vv (make-vv :value (pop keywords) :always t :used-p t))
+      (dolist (k keywords)
+        (insert-vv (make-vv :value k :always t :used-p t))))))
 
 
 (defun wt-vv-index (index permanent-p)

src/cmp/cmpbackend-cxx/cmpbackend-cxx.lsp
@@ -532,6 +532,9 @@ WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdS
 
 (defun compiler-pass/generate-cxx (c-pathname h-pathname data-pathname init-name source)
   (with-cxx-env ()
+    ;; After this step we still can add new objects, but objects that are
+    ;; already stored in VV or VVtemp must not change the location.
+    (optimize-cxx-data *referenced-objects*)
     (setq *compiler-phase* 't2)
     (with-open-file (*compiler-output1* c-pathname :direction :output
                                                    :if-does-not-exist :create

src/cmp/cmpbackend-cxx/cmpc-util.lsp
@@ -58,6 +58,26 @@
 (defvar *exit*)
 (defvar *unwind-exit*)
 
+;; T/NIL flag to determine whether one may generate lisp constant values as C
+;; structs.
+(defvar *use-static-constants-p*
+  #+ecl-min t #-ecl-min nil)
+
+;;; Constants that can be built as C values.
+(defvar *static-constants*)          ; holds { ( object c-variable constant ) }*
+
+;;; Pairs for inlining constants.
+(defvar *optimizable-constants*)        ; hoolds { (value . c1form) }*
+
+
+;;; Permanent objects are stored in VV[] that is available for the whole module
+;;; during the whole lifetime of the FASL. Temporary objects are stored in
+;;; VVtemp[] that is allocated in the initialization function and released after
+;;; the FASL is initialized.
+
+(defvar *permanent-objects*)            ; holds { vv-record }*
+(defvar *temporary-objects*)            ; holds { vv-record }*
+
 ;;; ----------------------------------------------------------------------
 ;;; CONVENIENCE FUNCTIONS / MACROS
 ;;;
@@ -72,7 +92,11 @@
          (*inline-information*
            (ext:if-let ((r (machine-inline-information *machine*)))
              (si:copy-hash-table r)
-             (make-inline-information *machine*))))
+             (make-inline-information *machine*)))
+         (*static-constants* nil)
+         (*optimizable-constants* (make-optimizable-constants *machine*))
+         (*permanent-objects* (make-array 128 :adjustable t :fill-pointer 0))
+         (*temporary-objects* (make-array 128 :adjustable t :fill-pointer 0)))
      ,@body))
 
 (defun-cached env-var-name (n) eql

src/cmp/cmpbackend-cxx/cmppass2-call.lsp
@@ -269,7 +269,7 @@
 ;;; This version is correct but unnecessarily slow - it goes through
 ;;; ecl_function_dispatch. wt-fdefinition handles all proper names.
 (defun call-unknown-global-fun (fname args)
-  `(CALL-INDIRECT ,(add-fname fname) ,(coerce-locs args) ,fname nil))
+  `(CALL-INDIRECT ,(get-object fname) ,(coerce-locs args) ,fname nil))
 
 ;;;
 ;;; call-stack-loc

src/cmp/cmpbackend-cxx/cmppass2-cont.lsp
@@ -51,7 +51,7 @@
       (progn
         (let ((*destination* 'VALUES))
           (c2expr* val))
-        (let ((name (add-symbol (blk-name blk))))
+        (let ((name (get-object (blk-name blk))))
           (wt-nl "cl_return_from(" (blk-var blk) "," name ");")))
       (let ((*destination* (blk-destination blk))
             (*exit* (blk-exit blk)))

src/cmp/cmpbackend-cxx/cmppass2-fun.lsp
@@ -198,13 +198,13 @@
   (mapc #'bind required-lcls requireds)
 
   (when fname-in-ihs-p
-    (open-inline-block)
-    (setf *ihs-used-p* t)
-    (push 'IHS *unwind-exit*)
-    (when (policy-debug-variable-bindings)
-      (build-debug-lexical-env (reverse requireds) t))
-    (wt-nl "ecl_ihs_push(cl_env_copy,&ihs," (add-fname (or description fname))
-           ",_ecl_debug_env);"))
+    (let ((fname (get-object (or description fname))))
+      (open-inline-block)
+      (setf *ihs-used-p* t)
+      (push 'IHS *unwind-exit*)
+      (when (policy-debug-variable-bindings)
+        (build-debug-lexical-env (reverse requireds) t))
+      (wt-nl "ecl_ihs_push(cl_env_copy,&ihs," fname ",_ecl_debug_env);")))
 
   ;; Bind optional parameters as long as there remain arguments.
   (when optionals
@@ -309,7 +309,7 @@
  (when (and (policy-check-nargs) use-narg)
    (flet ((wrong-num-arguments ()
             (if fname
-                (wt " FEwrong_num_arguments(" (add-symbol fname) ");")
+                (wt " FEwrong_num_arguments(" (get-object fname) ");")
                 (wt " FEwrong_num_arguments_anonym();"))))
      (if (and maxarg (= minarg maxarg))
          (progn (wt-nl "if (ecl_unlikely(narg!=" minarg "))")

src/cmp/cmpbackend-cxx/cmppass2-loc.lsp
@@ -170,18 +170,18 @@
                         (functionp (fdefinition fun-name))))))
     (cond
       ((not safe)
-       (let ((vv (add-fname fun-name)))
+       (let ((vv (get-object fun-name)))
          (wt "ecl_fdefinition(" vv ")")))
       ((eq name fun-name)
        ;; #'symbol
-       (let ((vv (add-fname name)))
+       (let ((vv (get-object name)))
          (wt "(" vv "->symbol.gfdef)")))
       (t
        ;; #'(SETF symbol)
        (let ((set-loc (assoc name *setf-definitions*)))
          (unless set-loc
-           (let* ((setf-vv (data-empty-loc))
-                  (name-vv (add-symbol name)))
+           (let* ((setf-vv (data-empty-loc*))
+                  (name-vv (get-object name)))
              (setf set-loc (list name setf-vv name-vv))
              (push set-loc *setf-definitions*)))
          (wt "ECL_CONS_CAR(" (second set-loc) ")"))))))

src/cmp/cmpbackend-cxx/cmppass2-top.lsp
@@ -124,9 +124,9 @@
     (wt-nl-h "/*")
     (wt-nl-h " * Statically defined constants")
     (wt-nl-h " */")
-    (loop for (value name builder) in (reverse *static-constants*)
+    (loop for (value vv builder) in (reverse *static-constants*)
           do (terpri *compiler-output2*)
-          do (funcall builder name value *compiler-output2*)))
+          do (funcall builder (vv-location vv) value *compiler-output2*)))
 
   (output-cfuns *compiler-output2*)
 
@@ -494,7 +494,7 @@
     (return-from c2fset
       (c2call-global c1form 'SI:FSET c1forms)))
   (let ((*inline-blocks* 0)
-        (loc (data-empty-loc)))
+        (loc (data-empty-loc*)))
     (push (list loc fname fun) *global-cfuns-array*)
     ;; FIXME! Look at C2LOCALS!
     (new-local fun)

commit 3c75b5b4d85ebb75fc5c2cca8bf11dc3c0d7f804

cmp: fix typo
Introduced by the refactor in commit 2304da9a830a6a00272bd086c11fc55980018d1b.

src/cmp/cmpbackend-cxx/cmppass2-data.lsp
@@ -328,6 +328,7 @@
 (defun search-vv (object &key permanent (errorp t))
   (let* ((test (if si:*compiler-constants* 'eq 'equal-with-circularity))
          (item (if permanent
+                   (find object *permanent-objects* :test test :key #'vv-value)
                    (or (find object *permanent-objects* :test test :key #'vv-value)
                        (find object *temporary-objects* :test test :key #'vv-value)))))
     (when (and (null item) errorp)