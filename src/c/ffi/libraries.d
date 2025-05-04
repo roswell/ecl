f69ded9777c43df20021788d2a4bfc0e9c6b4566
Marius Gerbershagen <marius.gerbershagen@gmail.com>
2025-05-04 18:02:59 +0200

    remove bogus `#undef bool` statement
    Not sure why that was ever there.

--- src/c/ffi/libraries.d
+++ src/c/ffi/libraries.d
@@ -44,9 +44,6 @@
 # ifdef HAVE_DLFCN_H
 #  include <dlfcn.h>
 #  define INIT_PREFIX "init_fas_"
-#  ifdef bool
-#   undef bool
-#  endif
 # endif
 # ifdef HAVE_MACH_O_DYLD_H
 #  include <mach-o/dyld.h>
