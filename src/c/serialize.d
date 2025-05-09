885e53a0b5cb1f4b3237f58fe0444195419bd115
Daniel Kochmański <daniel@turtleware.eu>
2025-05-09 11:34:33 +0200

    serialize: fix a braino
    ecl_frame was used instead of ecl_stack_frame to compute the size of t_frame.

--- src/c/serialize.d
+++ src/c/serialize.d
@@ -75,7 +75,7 @@ static cl_index object_size[] = {
 #endif
   ROUNDED_SIZE(ecl_codeblock), /* t_codeblock */
   ROUNDED_SIZE(ecl_foreign), /* t_foreign */
-  ROUNDED_SIZE(ecl_frame), /* t_frame */
+  ROUNDED_SIZE(ecl_stack_frame), /* t_frame */
   ROUNDED_SIZE(ecl_weak_pointer) /* t_weak_pointer */
 #ifdef ECL_SSE2
   , ROUNDED_SIZE(ecl_sse_pack) /* t_sse_pack */
