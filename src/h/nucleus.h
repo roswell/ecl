26c51461d5861dd45a5db9951cb5a81df28a696b
Daniel Kochmański <daniel@turtleware.eu>
2025-05-14 11:05:02 +0200

    nucleus: move ecl_core_struct to nucleus

--- src/h/nucleus.h
+++ src/h/nucleus.h
@@ -6,4 +6,31 @@
 
 #include "external.h"
 
+struct ecl_core_struct {
+  cl_env_ptr first_env;
+#ifdef ECL_THREADS
+  cl_object processes;
+  ecl_mutex_t processes_lock;
+  ecl_mutex_t global_lock;
+  ecl_mutex_t error_lock;
+  ecl_rwlock_t global_env_lock;
+  cl_index last_var_index;
+  cl_object reused_indices;
+#endif
+  size_t max_heap_size;
+  cl_object bytes_consed;
+  cl_object gc_counter;
+  bool gc_stats;
+  char *safety_region;
+
+  cl_index default_sigmask_bytes;
+  cl_object known_signals;
+
+  int path_max;
+  cl_object pathname_translations;
+
+  cl_object libraries;
+  cl_object library_pathname;
+};
+
 #endif  /* ECL_NUCLEUS_H */
