roswell/ecl

commit 0f4e9b1e17a6b0aca7fb3f4531b3acc2788fe6e6

core: factor out process managament from thread managament
This is a preliminary step towards coroutines and for switching global env in
single-threaded builds.

src/c/main.d
@@ -483,6 +483,7 @@ cl_boot(int argc, char **argv)
   setbuf(stdin,  stdin_buf);
   setbuf(stdout, stdout_buf);
 #endif
+  init_process();
 
   ARGC = argc;
   ARGV = argv;
@@ -492,7 +493,9 @@ cl_boot(int argc, char **argv)
   init_alloc();
   GC_disable();
   env = cl_core.first_env;
-  init_threads(env);
+#ifdef ECL_THREADS
+  init_threads();
+#endif
 
   /*
    * 1) Initialize symbols and packages

msvc/c/Makefile
@@ -12,7 +12,7 @@ ECL_FPE_CODE=fpe_x86.c
 
 !if "$(ECL_THREADS)" != ""
 ECL_THREADS_FLAG=1
-THREADS_OBJ= process.obj mutex.obj condition_variable.obj rwlock.obj \
+THREADS_OBJ= thread.obj mutex.obj condition_variable.obj rwlock.obj \
              semaphore.obj barrier.obj mailbox.obj atomic.obj
 !else
 ECL_THREADS_FLAG=0
@@ -111,7 +111,7 @@ OBJS	= main.obj symbol.obj package.obj cons.obj list.obj\
 	time.obj unixint.obj memory.obj \
 	mapfun.obj multival.obj hash.obj format.obj pathname.obj\
 	structure.obj load.obj unixfsys.obj unixsys.obj \
-	ffi.obj alloc_2.obj tcp.obj $(THREADS_OBJ) serialize.obj \
+	ffi.obj alloc_2.obj tcp.obj $(THREADS_OBJ) process.obj serialize.obj \
 	$(ECL_UCD_OBJ) $(ECL_SSE_OBJ)
 
 all:	$(DPP) ..\eclmin.lib ..\cinit.obj

src/aclocal.m4
@@ -267,7 +267,7 @@ THREAD_CFLAGS=''
 THREAD_LIBS=''
 THREAD_GC_FLAGS='--enable-threads=posix'
 INSTALL_TARGET='install'
-THREAD_OBJ="$THREAD_OBJ threads/process threads/mutex threads/condition_variable threads/semaphore threads/barrier threads/mailbox threads/rwlock"
+THREAD_OBJ="$THREAD_OBJ threads/thread threads/mutex threads/condition_variable threads/semaphore threads/barrier threads/mailbox threads/rwlock"
 clibs='-lm'
 SONAME=''
 SONAME_LDFLAGS=''

src/c/Makefile.in
@@ -75,7 +75,7 @@ OBJS = main.o symbol.o package.o cons.o list.o apply.o eval.o      \
 	cmpaux.o macros.o backq.o stacks.o time.o unixint.o mapfun.o    \
 	multival.o hash.o format.o pathname.o structure.o load.o        \
 	unixfsys.o unixsys.o serialize.o ffi.o sse2.o @EXTRA_OBJS@      \
-	threads/atomic.o
+	threads/atomic.o process.o
 
 .PHONY: all
 

src/c/process.d
@@ -1,0 +1,104 @@
+/* -*- Mode: C; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+/* vim: set filetype=c tabstop=2 shiftwidth=2 expandtab: */
+
+/*
+ * process.c - managing the process environment(s)
+ *
+ * Copyright (c) 2003 Juan Jose Garcia Ripoll
+ * Copyright (c) 2023 Daniel Kochmański
+ *
+ * See file 'LICENSE' for the copyright details.
+ *
+ */
+
+#define ECL_INCLUDE_MATH_H
+#include <ecl/ecl.h>            /* includes ECL_WINDOWS_THREADS */
+#include <ecl/internal.h>
+#include <ecl/ecl-inl.h>
+
+#ifndef __sun__ /* See unixinit.d for this */
+#define _XOPEN_SOURCE 600       /* For pthread mutex attributes */
+#endif
+#include <errno.h>
+#include <time.h>
+#include <signal.h>
+#include <string.h>
+#ifdef ECL_WINDOWS_THREADS
+# include <windows.h>
+#else
+# include <pthread.h>
+#endif
+#ifdef HAVE_GETTIMEOFDAY
+# include <sys/time.h>
+#endif
+#ifdef HAVE_SCHED_H
+# include <sched.h>
+#endif
+
+/* -- Thread-local variables ------------------------------------------------ */
+
+#ifdef ECL_THREADS
+# ifdef ECL_WINDOWS_THREADS
+#  define ecl_process_key_t DWORD
+#  define ecl_process_key_create(key) key = TlsAlloc()
+#  define ecl_process_get_tls(key) TlsGetValue(key)
+#  define ecl_process_set_tls(key,val) (TlsSetValue(key,val)!=0)
+# else
+#  define ecl_process_key_t static pthread_key_t
+#  define ecl_process_key_create(key) pthread_key_create(&key, NULL)
+#  define ecl_process_get_tls(key) pthread_getspecific(key)
+#  define ecl_process_set_tls(key,val) (pthread_setspecific(key,val)==0)
+# endif  /* ECL_WINDOWS_THREADS */
+
+/* Accessing a thread-local variable representing the environment. */
+
+ecl_process_key_t cl_env_key;
+
+cl_env_ptr
+ecl_process_env_unsafe(void)
+{
+  return ecl_process_get_tls(cl_env_key);
+}
+
+cl_env_ptr
+ecl_process_env(void)
+{
+  cl_env_ptr rv = ecl_process_get_tls(cl_env_key);
+  if(!rv) {
+    ecl_thread_internal_error("pthread_getspecific() failed.");
+  }
+  return rv;
+}
+
+void
+ecl_set_process_env(cl_env_ptr env)
+{
+  if(!ecl_process_set_tls(cl_env_key, env)) {
+    ecl_thread_internal_error("pthread_setspecific() failed.");
+  }
+}
+#else
+/* The current global environment for single-threaded builds. */
+cl_env_ptr cl_env_p = NULL;
+#endif  /* ECL_THREADS */
+
+/* -- Initialiation --------------------------------------------------------- */
+
+void
+init_process(void)
+{
+  cl_env_ptr env = cl_core.first_env;
+#ifdef ECL_THREADS
+  ecl_process_key_create(cl_env_key);
+  ecl_mutex_init(&cl_core.processes_lock, 1);
+  ecl_mutex_init(&cl_core.global_lock, 1);
+  ecl_mutex_init(&cl_core.error_lock, 1);
+  ecl_rwlock_init(&cl_core.global_env_lock);
+#endif
+  ecl_set_process_env(env);
+  env->default_sigmask = NULL;
+  env->method_cache = NULL;
+  env->slot_cache = NULL;
+  env->interrupt_struct = NULL;
+  env->disable_interrupts = 1;
+}

commit 9457813343a80c4041c2d1e997c2e3dc530f8c6f

fix initialization of the main thread environment
The default sigmask was NULL previously (bug introduced in the
refactor in commit 0f4e9b1e1).

src/c/main.d
@@ -192,6 +192,7 @@ init_env_aux(cl_env_ptr env)
 void
 ecl_init_first_env(cl_env_ptr env)
 {
+  env->default_sigmask = cl_core.default_sigmask;
 #ifdef ECL_THREADS
   init_threads();
 #endif