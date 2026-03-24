roswell/ecl

commit ede2f0aaa194ada48525f13a5aa2e3773d8c0143

cmp: define constant VM before it is first used in a produced header
When we've built ECL without dynamic VV, the preprocessor define VM was
referenced in the produced header before it was defined. That lead to an error.

src/cmp/cmpbackend-cxx/cmppass2-top.lsp
@@ -111,14 +111,6 @@
 (defun ctop-write (init-name h-pathname data-pathname &aux top-output-string)
   (wt-nl "#include \"" (brief-namestring h-pathname) "\"")
 
-  ;; VV might be needed by functions in CLINES.
-  (wt-nl-h "#ifdef ECL_DYNAMIC_VV")
-  (wt-nl-h "static cl_object *VV;")
-  (wt-nl-h "#else")
-  (wt-nl-h "static cl_object VV[VM];")
-  (wt-nl-h "#endif")
-  (output-clines *compiler-output2*)
-
   (wt-nl-h "#ifdef __cplusplus")
   (wt-nl-h "extern \"C\" {")
   (wt-nl-h "#endif")
@@ -141,7 +133,13 @@
         (progn
           (wt-nl-h "#define VM " (data-permanent-storage-size))
           (wt-nl-h "#define VMtemp "  (data-temporary-storage-size)))))
-
+  ;; VV might be needed by functions in CLINES.
+  (wt-nl-h "# ifdef ECL_DYNAMIC_VV")
+  (wt-nl-h "static cl_object *VV;")
+  (wt-nl-h "# else")
+  (wt-nl-h "static cl_object VV[VM];")
+  (wt-nl-h "# endif")
+  ;;
   (wt-nl-h "#define ECL_DEFINE_SETF_FUNCTIONS ")
   (loop for (name setf-vv name-vv) in *setf-definitions*
         do (wt-h #\\ #\Newline setf-vv "=ecl_setf_definition(" name-vv ",ECL_T);"))
@@ -159,6 +157,7 @@
           do (funcall builder (vv-location vv) value *compiler-output2*)))
 
   (output-cfuns *compiler-output2*)
+  (output-clines *compiler-output2*)
 
   (setq *compiler-phase* 't3)
 

commit 7e63eaf3eb18e5c805b856d7dbbd905e0d3e79ea

cmp: fix compilation of empty file
This regressed after commit ede2f0aaa194ada48525f13a5aa2e3773d8c0143

src/cmp/cmpbackend-cxx/cmppass2-top.lsp
@@ -132,13 +132,13 @@
           (wt-nl-h "#define VV NULL"))
         (progn
           (wt-nl-h "#define VM " (data-permanent-storage-size))
-          (wt-nl-h "#define VMtemp "  (data-temporary-storage-size)))))
-  ;; VV might be needed by functions in CLINES.
-  (wt-nl-h "# ifdef ECL_DYNAMIC_VV")
-  (wt-nl-h "static cl_object *VV;")
-  (wt-nl-h "# else")
-  (wt-nl-h "static cl_object VV[VM];")
-  (wt-nl-h "# endif")
+          (wt-nl-h "#define VMtemp "  (data-temporary-storage-size))
+          ;; VV might be needed by functions in CLINES.
+          (wt-nl-h "# ifdef ECL_DYNAMIC_VV")
+          (wt-nl-h "static cl_object *VV;")
+          (wt-nl-h "# else")
+          (wt-nl-h "static cl_object VV[VM];")
+          (wt-nl-h "# endif"))))
   ;;
   (wt-nl-h "#define ECL_DEFINE_SETF_FUNCTIONS ")
   (loop for (name setf-vv name-vv) in *setf-definitions*

src/tests/normal-tests/compiler.lsp
@@ -2701,3 +2701,14 @@
     (delete-file ofile)
     (is (equal (concatenate 'string part-1 part-2)
                *nested-eval-when-0114*))))
+
+;;; Date 2026-03-24
+;;; Description
+;;;
+;;;     Check that we can compile an empty file
+;;;
+(test cmp.0115.empty-file
+  (let* ((ofile
+          (with-compiler ("empty-file-0115.lsp" :load t))))
+    (delete-file "empty-file-0115.lsp")
+    (delete-file ofile)))