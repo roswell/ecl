roswell/ecl

commit 024a59b19887de062fbdb44a2b62043b52fa198b

core: move defacto constants from cl_core structure to global space

src/c/main.d
@@ -50,6 +50,26 @@
 
 const char *ecl_self;
 
+/* -- constants ----------------------------------------------------- */
+
+const cl_object ecl_ct_Jan1st1970UT = ecl_make_fixnum(39052800);
+
+ecl_def_ct_base_string(ecl_ct_null_string,"",0,,const);
+
+ecl_def_ct_single_float(ecl_ct_default_rehash_size,1.5f,,const);
+ecl_def_ct_single_float(ecl_ct_default_rehash_threshold,0.75f,,const);
+
+ecl_def_ct_single_float(ecl_ct_singlefloat_zero,0,,const);
+ecl_def_ct_double_float(ecl_ct_doublefloat_zero,0,,const);
+ecl_def_ct_long_float(ecl_ct_longfloat_zero,0,,const);
+
+ecl_def_ct_single_float(ecl_ct_singlefloat_minus_zero,-0.0,,const);
+ecl_def_ct_double_float(ecl_ct_doublefloat_minus_zero,-0.0,,const);
+ecl_def_ct_long_float(ecl_ct_longfloat_minus_zero,-0.0l,,const);
+
+ecl_def_ct_ratio(ecl_ct_plus_half,ecl_make_fixnum(1),ecl_make_fixnum(2),,const);
+ecl_def_ct_ratio(ecl_ct_minus_half,ecl_make_fixnum(-1),ecl_make_fixnum(2),,const);
+
 /************************ GLOBAL INITIALIZATION ***********************/
 
 
@@ -312,8 +332,6 @@ cl_shutdown(void)
   ecl_set_option(ECL_OPT_BOOTED, -1);
 }
 
-ecl_def_ct_single_float(default_rehash_size,1.5f,static,const);
-ecl_def_ct_single_float(default_rehash_threshold,0.75f,static,const);
 ecl_def_ct_base_string(str_common_lisp,"COMMON-LISP",11,static,const);
 ecl_def_ct_base_string(str_common_lisp_user,"COMMON-LISP-USER",16,static,const);
 ecl_def_ct_base_string(str_cl,"CL",2,static,const);
@@ -336,7 +354,6 @@ ecl_def_ct_base_string(str_gray,"GRAY",4,static,const);
 #endif
 ecl_def_ct_base_string(str_star_dot_star,"*.*",3,static,const);
 ecl_def_ct_base_string(str_rel_star_dot_star,"./*.*",5,static,const);
-ecl_def_ct_base_string(str_empty,"",0,static,const);
 ecl_def_ct_base_string(str_G,"G",1,static,const);
 ecl_def_ct_base_string(str_T,"T",1,static,const);
 #ifdef ENABLE_DLOPEN
@@ -351,22 +368,6 @@ ecl_def_ct_base_string(str_lsp,"lsp",3,static,const);
 ecl_def_ct_base_string(str_LSP,"LSP",3,static,const);
 ecl_def_ct_base_string(str_lisp,"lisp",4,static,const);
 ecl_def_ct_base_string(str_NIL,"NIL",3,static,const);
-ecl_def_ct_base_string(str_slash,"/",1,static,const);
-
-ecl_def_ct_single_float(flt_zero,0,static,const);
-ecl_def_ct_single_float(flt_zero_neg,-0.0,static,const);
-ecl_def_ct_double_float(dbl_zero,0,static,const);
-ecl_def_ct_double_float(dbl_zero_neg,-0.0,static,const);
-ecl_def_ct_long_float(ldbl_zero,0,static,const);
-ecl_def_ct_long_float(ldbl_zero_neg,-0.0l,static,const);
-ecl_def_ct_ratio(plus_half,ecl_make_fixnum(1),ecl_make_fixnum(2),static,const);
-ecl_def_ct_ratio(minus_half,ecl_make_fixnum(-1),ecl_make_fixnum(2),static,const);
-ecl_def_ct_single_float(flt_one,1,static,const);
-ecl_def_ct_single_float(flt_one_neg,-1,static,const);
-ecl_def_ct_single_float(flt_two,2,static,const);
-ecl_def_ct_complex(flt_imag_unit,&flt_zero_data,&flt_one_data,static,const);
-ecl_def_ct_complex(flt_imag_unit_neg,&flt_zero_data,&flt_one_neg_data,static,const);
-ecl_def_ct_complex(flt_imag_two,&flt_zero_data,&flt_two_data,static,const);
 
 struct cl_core_struct cl_core = {
   .packages = ECL_NIL,
@@ -396,25 +397,10 @@ struct cl_core_struct cl_core = {
   .compiler_readtable = ECL_NIL,
 
   .char_names = ECL_NIL,
-  .null_string = (cl_object)&str_empty_data,
 
-  .plus_half = (cl_object)&plus_half_data,
-  .minus_half = (cl_object)&minus_half_data,
-  .imag_unit = (cl_object)&flt_imag_unit_data,
-  .minus_imag_unit = (cl_object)&flt_imag_unit_neg_data,
-  .imag_two = (cl_object)&flt_imag_two_data,
-  .singlefloat_zero = (cl_object)&flt_zero_data,
-  .doublefloat_zero = (cl_object)&dbl_zero_data,
-  .singlefloat_minus_zero = (cl_object)&flt_zero_neg_data,
-  .doublefloat_minus_zero = (cl_object)&dbl_zero_neg_data,
-  .longfloat_zero = (cl_object)&ldbl_zero_data,
-  .longfloat_minus_zero = (cl_object)&ldbl_zero_neg_data,
-
-  .gensym_prefix = (cl_object)&str_G_data,
-  .gentemp_prefix = (cl_object)&str_T_data,
+  .gensym_prefix = ECL_NIL,
+  .gentemp_prefix = ECL_NIL,
   .gentemp_counter = ecl_make_fixnum(0),
-
-  .Jan1st1970UT = ECL_NIL,
 
   .system_properties = ECL_NIL,
 
@@ -422,9 +408,8 @@ struct cl_core_struct cl_core = {
 #ifdef ECL_THREADS
   .processes = ECL_NIL,
 #endif
-  /* LIBRARIES is an adjustable vector of objects. It behaves as
-     a vector of weak pointers thanks to the magic in
-     gbc.d/alloc_2.d */
+  /* LIBRARIES is an adjustable vector of objects. It behaves as a vector of
+     weak pointers thanks to the magic in the garbage collector. */
   .libraries = ECL_NIL,
 
   .max_heap_size = 0,
@@ -443,12 +428,7 @@ struct cl_core_struct cl_core = {
   .last_var_index = 0,
   .reused_indices = ECL_NIL,
 #endif
-  .slash = (cl_object)&str_slash_data,
-
   .compiler_dispatch = ECL_NIL,
-
-  .rehash_size = (cl_object)&default_rehash_size_data,
-  .rehash_threshold = (cl_object)&default_rehash_threshold_data,
 
   .known_signals = ECL_NIL
 };
@@ -560,6 +540,8 @@ cl_boot(int argc, char **argv)
 #else
   cl_core.path_max = MAXPATHLEN;
 #endif
+  cl_core.gensym_prefix = (cl_object)&str_G_data;
+  cl_core.gentemp_prefix = (cl_object)&str_T_data;
 
   cl_core.lisp_package =
     ecl_make_package(str_common_lisp,
@@ -650,8 +632,8 @@ cl_boot(int argc, char **argv)
    */
   cl_core.char_names = aux =
     cl__make_hash_table(@'equalp', ecl_make_fixnum(128), /* size */
-                        cl_core.rehash_size,
-                        cl_core.rehash_threshold);
+                        ecl_ct_default_rehash_size,
+                        ecl_ct_default_rehash_threshold);
   for (i = 0; char_names[i].elt.self; i++) {
     cl_object name = (cl_object)(char_names + i);
     cl_object code = ecl_make_fixnum(i);
@@ -677,8 +659,8 @@ cl_boot(int argc, char **argv)
    */
   cl_core.system_properties =
     cl__make_hash_table(@'equal', ecl_make_fixnum(1024), /* size */
-                        cl_core.rehash_size,
-                        cl_core.rehash_threshold);
+                        ecl_ct_default_rehash_size,
+                        ecl_ct_default_rehash_threshold);
 
   ECL_SET(@'*random-state*', ecl_make_random_state(ECL_T));
 
@@ -744,8 +726,8 @@ cl_boot(int argc, char **argv)
    */
   ECL_SET(@'si::*class-name-hash-table*',
           cl__make_hash_table(@'eq', ecl_make_fixnum(1024), /* size */
-                              cl_core.rehash_size,
-                              cl_core.rehash_threshold));
+                              ecl_ct_default_rehash_size,
+                              ecl_ct_default_rehash_threshold));
 
   /*
    * Features.

src/c/compiler.d
@@ -3107,10 +3107,11 @@ c_cons_cdr(cl_env_ptr env, cl_object args, int flags)
 cl_object
 si_need_to_make_load_form_p(cl_object object)
 {
-  cl_object load_form_cache = cl__make_hash_table(@'eq',
-                                                  ecl_make_fixnum(16),
-                                                  cl_core.rehash_size,
-                                                  cl_core.rehash_threshold);
+  cl_object load_form_cache =
+    cl__make_hash_table(@'eq',
+                        ecl_make_fixnum(16),
+                        ecl_ct_default_rehash_size,
+                        ecl_ct_default_rehash_threshold);
   cl_object waiting_objects = ecl_list1(object);
   cl_type type = t_start;
 
@@ -3803,8 +3804,8 @@ init_compiler()
   cl_object dispatch_table =
     cl_core.compiler_dispatch =
     cl__make_hash_table(@'eq', ecl_make_fixnum(128), /* size */
-                        cl_core.rehash_size,
-                        cl_core.rehash_threshold);
+                        ecl_ct_default_rehash_size,
+                        ecl_ct_default_rehash_threshold);
   int i;
   for (i = 0; database[i].symbol; i++) {
     ecl_sethash(database[i].symbol, dispatch_table, ecl_make_fixnum(i));

src/c/hash.d
@@ -943,8 +943,8 @@ ecl_extend_hashtable(cl_object hashtable)
                          (weakness ECL_NIL)
                          (synchronized ECL_NIL)
                          (size ecl_make_fixnum(1024))
-                         (rehash_size cl_core.rehash_size)
-                         (rehash_threshold cl_core.rehash_threshold))
+                         (rehash_size ecl_ct_default_rehash_size)
+                         (rehash_threshold ecl_ct_default_rehash_threshold))
 @ {
     cl_object hash = cl__make_hash_table(test, size, rehash_size, rehash_threshold);
     if (hash->hash.test == ecl_htt_generic) {

src/c/num_co.d
@@ -431,21 +431,21 @@ cl_imagpart(cl_object x)
     break;
   case t_singlefloat:
     if (signbit(ecl_single_float(x)))
-      x = cl_core.singlefloat_minus_zero;
+      x = ecl_ct_singlefloat_minus_zero;
     else
-      x = cl_core.singlefloat_zero;
+      x = ecl_ct_singlefloat_zero;
     break;
   case t_doublefloat:
     if (signbit(ecl_double_float(x)))
-      x = cl_core.doublefloat_minus_zero;
+      x = ecl_ct_doublefloat_minus_zero;
     else
-      x = cl_core.doublefloat_zero;
+      x = ecl_ct_doublefloat_zero;
     break;
   case t_longfloat:
     if (signbit(ecl_long_float(x)))
-      x = cl_core.longfloat_minus_zero;
+      x = ecl_ct_longfloat_minus_zero;
     else
-      x = cl_core.longfloat_zero;
+      x = ecl_ct_longfloat_zero;
     break;
   case t_complex:
     x = x->gencomplex.imag;

src/c/number.d
@@ -501,9 +501,9 @@ ecl_make_single_float(float f)
   if (f == (float)0.0) {
 #if defined(ECL_SIGNED_ZERO)
     if (signbit(f))
-      return cl_core.singlefloat_minus_zero;
+      return ecl_ct_singlefloat_minus_zero;
 #endif
-    return cl_core.singlefloat_zero;
+    return ecl_ct_singlefloat_zero;
   }
   x = ecl_alloc_object(t_singlefloat);
   ecl_single_float(x) = f;
@@ -519,9 +519,9 @@ ecl_make_double_float(double f)
   if (f == (double)0.0) {
 #if defined(ECL_SIGNED_ZERO)
     if (signbit(f))
-      return cl_core.doublefloat_minus_zero;
+      return ecl_ct_doublefloat_minus_zero;
 #endif
-    return cl_core.doublefloat_zero;
+    return ecl_ct_doublefloat_zero;
   }
   x = ecl_alloc_object(t_doublefloat);
   ecl_double_float(x) = f;
@@ -537,9 +537,9 @@ ecl_make_long_float(long double f)
   if (f == (long double)0.0) {
 #if defined(ECL_SIGNED_ZERO)
     if (signbit(f))
-      return cl_core.longfloat_minus_zero;
+      return ecl_ct_longfloat_minus_zero;
 #endif
-    return cl_core.longfloat_zero;
+    return ecl_ct_longfloat_zero;
   }
   x = ecl_alloc_object(t_longfloat);
   x->longfloat.value = f;

src/c/numbers/atan.d
@@ -15,10 +15,18 @@
 
 #define ECL_INCLUDE_MATH_H
 #include <ecl/ecl.h>
+#include <ecl/ecl-inl.h>
 #include <ecl/internal.h>
 #include <ecl/impl/math_fenv.h>
 
 #pragma STDC FENV_ACCESS ON
+
+ecl_def_ct_single_float(ecl_ct_flt_zero,0,static,const);
+ecl_def_ct_single_float(ecl_ct_flt_one,1,static,const);
+ecl_def_ct_single_float(ecl_ct_flt_one_neg,-1,static,const);
+
+ecl_def_ct_complex(ecl_ct_imag_unit,ecl_ct_flt_zero,ecl_ct_flt_one,static,const);
+ecl_def_ct_complex(ecl_ct_minus_imag_unit,ecl_ct_flt_zero,ecl_ct_flt_one_neg,static,const);
 
 cl_object
 ecl_atan2(cl_object y, cl_object x)
@@ -53,20 +61,20 @@ ecl_atan1(cl_object y)
 {
   if (ECL_COMPLEXP(y)) {
 #if 0 /* ANSI states it should be this first part */
-    cl_object z = ecl_times(cl_core.imag_unit, y);
+    cl_object z = ecl_times(ecl_ct_imag_unit, y);
     z = ecl_plus(ecl_log1(ecl_one_plus(z)),
                  ecl_log1(ecl_minus(ecl_make_fixnum(1), z)));
     z = ecl_divide(z, ecl_times(ecl_make_fixnum(2),
-                                cl_core.imag_unit));
+                                ecl_ct_imag_unit));
 #else
-    cl_object z1, z = ecl_times(cl_core.imag_unit, y);
+    cl_object z1, z = ecl_times(ecl_ct_imag_unit, y);
     z = ecl_one_plus(z);
     z1 = ecl_times(y, y);
     z1 = ecl_one_plus(z1);
     z1 = ecl_sqrt(z1);
     z = ecl_divide(z, z1);
     z = ecl_log1(z);
-    z = ecl_times(cl_core.minus_imag_unit, z);
+    z = ecl_times(ecl_ct_minus_imag_unit, z);
 #endif /* ANSI */
     return z;
   } else {

src/c/numbers/round.d
@@ -87,12 +87,12 @@ ecl_round2_integer(const cl_env_ptr the_env, cl_object x, cl_object y, cl_object
   cl_object q1 = ecl_integer_divide(q->ratio.num, q->ratio.den);
   cl_object r = ecl_minus(q, q1);
   if (ecl_minusp(r)) {
-    int c = ecl_number_compare(cl_core.minus_half, r);
+    int c = ecl_number_compare(ecl_ct_minus_half, r);
     if (c > 0 || (c == 0 && ecl_oddp(q1))) {
       q1 = ecl_one_minus(q1);
     }
   } else {
-    int c = ecl_number_compare(r, cl_core.plus_half);
+    int c = ecl_number_compare(r, ecl_ct_plus_half);
     if (c > 0 || (c == 0 && ecl_oddp(q1))) {
       q1 = ecl_one_plus(q1);
     }

src/c/numbers/sqrt.d
@@ -76,7 +76,7 @@ ecl_sqrt_long_float(cl_object x)
 static cl_object
 ecl_sqrt_complex(cl_object x)
 {
-  return ecl_expt(x, cl_core.plus_half);
+  return ecl_expt(x, ecl_ct_plus_half);
 }
 
 #ifdef ECL_COMPLEX_FLOAT

commit ac5fed6bbde08d7df6f57412391181b9818a2efc

fix a regression in 024a59b19887de062fbdb44a2b62043b52fa198b
The problem found and fixed by Yuguo Zhang. Fixes #852.

src/c/main.d
@@ -103,7 +103,8 @@ ecl_boot(void)
 
 /* -- constants ----------------------------------------------------- */
 
-const cl_object ecl_ct_Jan1st1970UT = ecl_make_fixnum(39052800);
+/* (24*60*60)*(17+365*70) */
+const cl_object ecl_ct_Jan1st1970UT = ecl_make_fixnum(2208988800);
 
 ecl_def_ct_base_string(ecl_ct_null_string,"",0,,const);
 