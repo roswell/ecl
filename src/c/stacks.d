c6d22671a0c7e774c2f3dcc12f31ff1f5a5313c9
Marius Gerbershagen <marius.gerbershagen@gmail.com>
2026-08-21 19:48:11 +0200

    cc: don't turn off optimizations for clang
    It turned out that this was caused by mismatching alignment of our own
    data structures and not a clang bug.

--- src/c/stacks.d
+++ src/c/stacks.d
@@ -279,11 +279,6 @@ ecl_stack_frame_open(cl_env_ptr env, cl_object f, cl_index size)
   return f;
 }
 
-/* Some clang versions miscompile the following function on x86_64.
- * Temporarily turn off optimizations here. */
-#if defined(__clang__) && __clang_major__ >= 17 && __clang_major__ <= 22 && defined(__x86_64__)
-[[clang::optnone]]
-#endif
 void
 ecl_stack_frame_push(cl_object f, cl_object o)
 {
@@ -339,11 +334,6 @@ ecl_stack_frame_push_values(cl_object f)
   }
 }
 
-/* Some clang versions miscompile the following function on x86_64.
- * Temporarily turn off optimizations here. */
-#if defined(__clang__) && __clang_major__ >= 17 && __clang_major__ <= 22 && defined(__x86_64__)
-[[clang::optnone]]
-#endif
 cl_object
 ecl_stack_frame_pop_values(cl_object f)
 {
